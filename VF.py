1a832473e64a61bbcdeb8046135e313c7b6aa079
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-17 18:43:12 +0100

    Update VF.py

--- VF.py
+++ VF.py
@@ -166,6 +166,25 @@ def Check_Certificate(url, Counter_URL = 0):
             cert = x509.load_der_x509_certificate(cert_der, default_backend())
             print (cert.signature_hash_algorithm.name)
 
+def Check_Website(url, t_seconds):
+	from re import search
+	from requests import get
+	from sys import argv
+	from time import sleep
+
+	with open(argv[1], 'r') as f:
+		Text = f.read().splitlines()
+
+	with open('/opt/test.txt', 'w') as f:
+		for i in Text:
+			r = get(str(i), verify=False, timeout=(25,25))
+			x = search(r'Apache', str(r.content))
+			try:
+				Pos_Start, Pos_Ende = x.span()[0], x.span()[1]
+				f.write(f'{i} : {str(r.content)[Pos_Start:Pos_Ende+20].split("<")[0]}\n')
+			except: f.write(f'{i} : -\n')
+	
+	
 def Check_Security_Flags(url, t_seconds):
     r = get(url, timeout=(t_seconds, t_seconds), verify=False, allow_redirects=True)
 
