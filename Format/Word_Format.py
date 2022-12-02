fabe914e4ed697f29d598993086550fe011b034d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-02 22:54:03 +0100

    Update Word_Format.py

--- Format/Word_Format.py
+++ Format/Word_Format.py
@@ -1,14 +1,14 @@
-﻿#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer C. B. Herold
 
 # Libraries
 try:
-    from ..VF import *
+    from resources.VF import *
 except ImportError:
-    import sys
-    sys.path.append('.')
-    from VF import *
+    from sys import path as syspath
+    syspath.append('.')
+    from resources.VF import *
 
 def Word_Table(Dict_Result):
     try:
