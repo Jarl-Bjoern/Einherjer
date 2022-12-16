40b8b60c13163d93a5ef1f028daf8e3bafe4535d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-16 19:59:39 +0100

    Update Einherjer.py

--- Einherjer.py
+++ Einherjer.py
@@ -187,6 +187,9 @@ def main(Date, Dict_Result = {'Header': {}, 'Information': {}, 'SSH': {}, 'SSL':
     if (args.read_config_ssh_ciphers != None): pass
     if (args.read_config_ssl_ciphers != None): pass
 
+    if (args.add_http_proxy != None): Dict_Proxies['http'] = args.add_http_proxy
+    if (args.add_https_proxy != None): Dict_Proxies['https'] = args.add_https_proxy
+
     if (args.scan_all == False and args.scan_site_screenshot == False and args.scan_site_ssl == False and args.scan_site_header == False and args.scan_site_fuzzing == False and args.scan_ssh == False and args.scan_site_screenshot_recursive == False and args.scan_security_flags == False): Error_Message('The scanning method is missing!\n')
     elif (args.scan_all != False and args.scan_site_screenshot == False and args.scan_site_ssl == False and args.scan_site_header == False and args.scan_site_fuzzing == False and args.scan_ssh == False and args.scan_site_screenshot_recursive == False and args.scan_security_flags == False):
         Array_Switch.append(driver),Array_Switch.append(True),Array_Switch.append(True),Array_Switch.append(True),Array_Switch.append(True),Array_Switch.append(True),Array_Switch.append(True)
