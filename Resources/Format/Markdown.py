2db02e75ee64b5450eabbd71fd79b0d8449c809b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-19 17:35:46 +0100

    Create Markdown.py

---
+++ Resources/Format/Markdown.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Libraries
+from Resources.VF import *
+
+def Markdown_Table(Dict_Result, location, Array_Files = []):
+    pass
