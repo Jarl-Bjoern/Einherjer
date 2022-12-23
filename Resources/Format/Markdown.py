32e13cca30b33b3c9cf1359364f70f239a9516ef
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-23 12:47:47 +0100

    Update Markdown.py

--- Resources/Format/Markdown.py
+++ Resources/Format/Markdown.py
@@ -6,4 +6,15 @@
 from Resources.VF import *
 
 def Markdown_Table(Dict_Result, location, Array_Files = []):
-    pass
+    if (Dict_Result['Header'] != {}):
+        Array_Files.append(join(location, 'result_header.md'))
+        with open(join(location, 'result_header.md'), 'w', encoding='UTF-8', newline='') as md_file:
+            pass
+    if (Dict_Result['Information'] != {}):
+        pass
+    if (Dict_Result['Security_Flag'] != {}):
+        pass
+    if (Dict_Result['SSH'] != {}):
+        pass
+        
+    return Array_Files
