ee6c2a7e9079d7d1bd9c7c8da77da1522c656e90
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-26 17:57:40 +0100

    Update Markdown.py

--- Resources/Format/Markdown.py
+++ Resources/Format/Markdown.py
@@ -9,10 +9,11 @@ def Markdown_Table(Dict_Result, location, Array_Files = []):
     if (Dict_Result['Header'] != {}):
         Array_Files.append(join(location, 'result_header.md'))
         with open(join(location, 'result_header.md'), 'w', encoding='UTF-8', newline='') as md_file:
-            md_file.write('| URL | DNS | X-FRAME-OPTIONS | X-XSS-PROTECTION | CONTENT-SECURITY-POLICY | STRICT-TRANSPORT-SECURITY | X-CONTENT-TYPE-OPTIONS | REFERRER-POLICY |')
-            md_file.write('| --- | --- | --------------- | ---------------- | ----------------------- | ------------------------- | ---------------------- | --------------- |')
+            md_file.write('| URL | DNS | X-FRAME-OPTIONS | X-XSS-PROTECTION | CONTENT-SECURITY-POLICY | STRICT-TRANSPORT-SECURITY | X-CONTENT-TYPE-OPTIONS | REFERRER-POLICY |\n')
+            md_file.write('| --- | --- | --------------- | ---------------- | ----------------------- | ------------------------- | ---------------------- | --------------- |\n')
             for Target in Dict_Result['Header']:
                 Temp_Word = ""
+                Temp_Word += f"| {Target} | "
                 for Result_Left, Result_Right in Dict_Result['Header'][Target].items():
                     if ((Result_Left == "X-XSS-PROTECTION") and (Result_Right == "1" or (Result_Right == "1; MODE=BLOCK"))): Result_Right = "FEHLT"
                     elif (Result_Left == "X-CONTENT-TYPE-OPTIONS" and Result_Right != "NOSNIFF"): Result_Right = "FEHLT"
@@ -27,10 +28,11 @@ def Markdown_Table(Dict_Result, location, Array_Files = []):
     if (Dict_Result['Information'] != {}):
         Array_Files.append(join(location, 'result_information_disclosure.md'))
         with open(join(location, 'result_information_disclosure.md'), 'w', encoding='UTF-8', newline='') as md_file:
-            md_file.write('| URL | DNS | X-POWERED-BY | SERVER |')
-            md_file.write('| --- | --- | ------------ | ------ |')
+            md_file.write('| URL | DNS | X-POWERED-BY | SERVER |\n')
+            md_file.write('| --- | --- | ------------ | ------ |\n')
             for Target in Dict_Result['Header']:
                 Temp_Word = ""
+                Temp_Word += f"| {Target} | "
                 for Result_Left, Result_Right in Dict_Result['Information'][Target].items():
                     if (Result_Left == "DNS" and Result_Right == ""): Result_Right = "FEHLT"
                     elif (Result_Left == Array_Information_Disclosure_Header[0] and Result_Right == ""): Result_Right = "FEHLT"
