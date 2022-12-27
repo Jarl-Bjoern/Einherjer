e996ed77581142de16f433e5e2b4ffd35b68e221
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-27 20:41:14 +0100

    Update XML.py

--- Resources/Format/XML.py
+++ Resources/Format/XML.py
@@ -3,7 +3,7 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Resources.VF import *
+from Resources.Variables_File import *
 
 def XML_Table(Dict_Result, location):
     try: import lxml.etree, lxml.builder
