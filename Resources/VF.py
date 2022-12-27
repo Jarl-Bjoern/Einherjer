7012b48c9d3173b56bff940cb6334514ef62108b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-27 20:16:35 +0100

    Update VF.py

--- Resources/VF.py
+++ Resources/VF.py
@@ -74,15 +74,6 @@ progress_columns = (
 )
 
 # Classes
-class Colors:
-    CYAN = '\033[36m'
-    GREEN = '\033[32m'
-    ORANGE = '\033[33m'
-    BLUE = '\033[34m'
-    RED = '\033[31m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
-
 class Standard:
     def Stdout_Output(Text_Array, Output_Seconds):
         for char in Text_Array:
