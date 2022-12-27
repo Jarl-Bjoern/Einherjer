e78fef237034bdce5d8aaf50d3a7c432fec3a751
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-27 21:12:06 +0100

    Update Scan_DNS.py

--- Resources/Workfiles/Scan_DNS.py
+++ Resources/Workfiles/Scan_DNS.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
+
+def Scan_DNS():
+  def DNS_Brute(domain, Array_Temp = [], Array_Result = []):
+          for _ in Wordlist:
+                  url = f'{_}.{domain}'
+                  r = get(url)
+                  if (r.status_code == "200"):
+                          if (url not in Array_Temp):
+                                  Array_Temp.append(url)
+                                  Array_Result.append(url)
+          return Array_Temp, Array_Result
+
+  Array_Temp, Array_Result = DNS_Brute("test.localdomain")
+  while True:
+          if (len(Array_Temp) != 0):
+                  for _ in Array_Temp:
+                          Array_Temp_Sec, Array_Result = DNS_Brute(_)
+          else:
+                  break
+          Array_Temp = Array_Temp_Sec
