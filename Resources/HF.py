f5df2bb392ec81877d6bacfe110ca2a415fd6a42
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-12-27 20:06:19 +0100

    Update HF.py

--- Resources/HF.py
+++ Resources/HF.py
@@ -43,3 +43,57 @@ try:
     from urllib3 import disable_warnings
     from urllib3.exceptions import *
 except ModuleNotFoundError as e: Module_Error(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'")
+
+# Argument_Parser
+from Resources.ArgParser import Argument_Parser
+args = Argument_Parser()
+del Argument_Parser
+
+# Scanning_Module_Filtering
+if (args.scan_all == False and args.scan_site_screenshot == False and args.scan_site_ssl == False and args.scan_site_header == False and args.scan_site_fuzzing == False and args.scan_ssh == False and args.scan_site_screenshot_recursive == False and args.scan_security_flags == False): Module_Error('The scanning method is missing!\n')
+elif (args.scan_all != False and args.scan_site_screenshot == False and args.scan_site_ssl == False and args.scan_site_header == False and args.scan_site_fuzzing == False and args.scan_ssh == False and args.scan_site_screenshot_recursive == False and args.scan_security_flags == False):
+    try:
+        from contextlib import redirect_stdout
+        from cv2 import countNonZero, imread, imwrite, rectangle, split as cvsplit, subtract
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.common.keys import Keys
+        from selenium.webdriver.chrome.service import Service
+        from selenium.webdriver.remote.webdriver import WebDriver
+        from webbrowser import open as webbrowser_open
+        with redirect_stdout(None):
+           from webdriver_manager.chrome import ChromeDriverManager
+        from asyncssh import Error as AsyncSSHError, get_server_auth_methods, SSHClient, SSHClientConnection
+        from cryptography import x509
+        from cryptography.hazmat.backends import default_backend
+        from paramiko.transport import Transport
+        from requests import get, Session
+        from socket import create_connection
+        import asyncio
+    except ModuleNotFoundError as e: Module_Error(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'")
+elif (args.scan_all == False):
+    try:
+        if (args.scan_site_screenshot != False):
+            from contextlib import redirect_stdout
+            from cv2 import countNonZero, imread, imwrite, rectangle, split as cvsplit, subtract
+            from selenium import webdriver
+            from selenium.webdriver.common.by import By
+            from selenium.webdriver.common.keys import Keys
+            from selenium.webdriver.chrome.service import Service
+            from selenium.webdriver.remote.webdriver import WebDriver
+            from webbrowser import open as webbrowser_open
+            with redirect_stdout(None):
+               from webdriver_manager.chrome import ChromeDriverManager
+        if (args.scan_site_ssl != False):
+            from cryptography import x509
+            from cryptography.hazmat.backends import default_backend
+            from socket import create_connection
+        if (args.scan_site_header != False or args.scan_site_fuzzing != False):
+            from requests import get
+        if (args.scan_ssh != False):
+            from asyncssh import Error as AsyncSSHError, get_server_auth_methods, SSHClient, SSHClientConnection
+            from paramiko.transport import Transport
+            import asyncio
+        if (args.scan_security_flags != False):
+            from requests import Session
+    except ModuleNotFoundError as e: Module_Error(f"The module was not found\n\n{e}\n\nPlease confirm with the button 'Return'")
