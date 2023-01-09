57e34782e87b3a443a25daf4241a75a9c443c34f
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-09 19:06:35 +0100

    Update Logs.py

--- Resources/Standard_Operations/Logs.py
+++ Resources/Standard_Operations/Logs.py
@@ -10,14 +10,14 @@ class Logs:
     def Error_Message(x):
         print(x), sleep(2), exit()
 
-    def Write_Log(url, host, Log_Path = dirname(realpath(__file__)).replace('Resources/Header_Files', 'Logs')):
+    def Write_Log(url, host, Log_Path = dirname(realpath(__file__)).replace('Resources/Standard_Operations', 'Logs')):
         if (not exists(Log_Path)): makedirs(Log_Path)
         with open(join(Log_Path, f'{Date}_failed-url.txt'), 'a') as f:
             f.write(f'{url}\n')
         if (host != ""): Logs.Log_File(Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.RED+'FAILED\n'+Colors.RESET)
         else: Logs.Log_File(Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.RED+'FAILED\n'+Colors.RESET)
 
-    def Log_File(Text, Log_Path = dirname(realpath(__file__)).replace('Resources/Header_Files', 'Logs')):
+    def Log_File(Text, Log_Path = dirname(realpath(__file__)).replace('Resources/Standard_Operations', 'Logs')):
         if (not exists(Log_Path)): makedirs(Log_Path)
         with open(join(Log_Path, f"{Date}.log"), "a") as f:
             f.write(Text)
