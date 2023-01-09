75dd7a461175cd487e3b70b9c7d34955348a7df3
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-09 16:45:04 +0100

    Update LaTeX.py

--- Resources/Format/LaTeX.py
+++ Resources/Format/LaTeX.py
@@ -3,7 +3,7 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Resources.Variables_File import *
+from Resources.Header_Files.Variables import *
 
 def Latex_Table(Dict_Result, location, Table_Body = ""):
     Define_Color = r"""\input{libs/packages.tex}
