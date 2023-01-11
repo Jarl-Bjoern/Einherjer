e024e73004b8049467a3db279eb07df42327572e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-11 13:57:16 +0100

    Update Colors.py

--- Resources/Colors.py
+++ Resources/Colors.py
@@ -8,5 +8,7 @@ class Colors:
     ORANGE = '\033[33m'
     BLUE = '\033[34m'
     RED = '\033[31m'
+    YELLOW = '\033[33m'
+    PURPLE = '\033[35m'
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
