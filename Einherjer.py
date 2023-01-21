32550fdebe0251cbdb01c90786c78d779a2c31f3
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-21 21:47:24 +0100

    Update Einherjer.py

--- Einherjer.py
+++ Einherjer.py
@@ -7,7 +7,7 @@ from Resources.Header_Files.Threads import *
 from Resources.Workfiles.Scan_Screen import Web
 
 # Main_Function
-def main(Date, args, Dict_Result = {'Header': {}, 'Information': {}, 'SSH': {}, 'SSL': {}, 'Fuzzing': {}, 'Security_Flag': {}}, Array_HTTP_Filter = [], Array_Switch = [], Array_Thread_Args = [], Dict_Switch = {}, Dict_Threads = {}, Counter_Connections = 0, Switch_Internet_Connection = False):
+def main(Date, args, Dict_Result = {'Header': {}, 'Information': {}, 'SSH': {}, 'SSL': {}, 'Fuzzing': {}, 'Security_Flag': {}}, Array_HTTP_Filter = [], Array_Switch = [], Array_Thread_Args = [], Dict_Switch = {}, Dict_Threads = {}, Counter_Connections = 0, Switch_Internet_Connection = False, Screen_Dir = ""):
     global Switch_nmap
 
     # Target_Options
