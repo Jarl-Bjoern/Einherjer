f2e14bb19bfbb0b8b7600cf31d9dddeea2361a02
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-23 18:01:23 +0100

    Update Threads.py

--- Resources/Header_Files/Threads.py
+++ Resources/Header_Files/Threads.py
@@ -17,11 +17,11 @@ def Thread_Scanning_Start(url, t_seconds, queue, driver_options, scan_ssl, scan_
         if (scan_header != False and '//' in url and 'http' in url):
             Dict_Result['Header'][url], Dict_Result['Information'][url] = Check_Site_Header(url, t_seconds, Host_Name)
         if (scan_ssl != False and '//' in url and 'http' in url):
-            Dict_Result['SSL'] = SSL_Vulns(url, t_seconds)
+            Dict_Result['SSL'][url] = SSL_Vulns(url, t_seconds)
         if (scan_security_flag != False and '//' in url and 'http' in url):
-            Dict_Result['Security_Flag'] = Check_Security_Flags(url, t_seconds, Host_Name)
+            Dict_Result['Security_Flag'][url] = Check_Security_Flags(url, t_seconds, Host_Name)
         if (scan_fuzzing != False and '//' in url and 'http' in url):
-            Dict_Result['Fuzzing'] = Check_Site_Paths(url, t_seconds)
+            Dict_Result['Fuzzing'][url] = Check_Site_Paths(url, t_seconds)
         if (scan_fuzzing_recurse != False and '//' in url and 'http' in url):
             pass
         if (scan_ssh != False and '//' in url and 'ssh' in url):
