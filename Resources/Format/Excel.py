0399e113647cf91c45176900d85cf366fd59ee02
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-23 18:33:40 +0100

    Update Excel.py

--- Resources/Format/Excel.py
+++ Resources/Format/Excel.py
@@ -45,7 +45,7 @@ def Excel_Table(Dict_Result, location, Array_Header_Letter = ['A','B','C','D','E
         # Design
         bold_text = workbook.add_format({'bold': True})
         rotate_text = workbook.add_format({'bold': True})
-        rotate_text.set_rotation(90)
+        rotate_text.set_rotation(-90)
         center_text = workbook.add_format()
         center_text.set_center_across()
         th_cell_format = workbook.add_format()
@@ -55,6 +55,7 @@ def Excel_Table(Dict_Result, location, Array_Header_Letter = ['A','B','C','D','E
 
         m = 1
         worksheet.write(f'A1', 'URL', bold_text)
+        worksheet.write(f'B1', 'DNS', bold_text)
         for Header in Array_Header:
             worksheet.write(f'{Array_Header_Letter[m]}1', Header, rotate_text)
             m += 1
