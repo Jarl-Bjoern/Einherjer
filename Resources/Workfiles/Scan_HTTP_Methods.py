394a60e5bcef30c6983e96fc4a683c287d4a571a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-30 20:39:13 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -3,7 +3,6 @@
 # Rainer Christian Bjoern Herold
 
 def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}):
-    import http.client
     r = HTTPSConnection(url)
     for Method in Array_HTTP_Methods:
         r.request(Method, "/")
