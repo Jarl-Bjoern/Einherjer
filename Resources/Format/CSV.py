4080ec86b6a978a17c301a2c1e5593f79f19d4a5
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-30 19:58:00 +0100

    Update CSV.py

--- Resources/Format/CSV.py
+++ Resources/Format/CSV.py
@@ -95,7 +95,7 @@ def CSV_Table(Dict_Result, location, Array_Files = []):
         Array_Files.append(join(location, f'result_certificate.csv'))
         with open(join(location, f'result_certificate.csv'), 'w', encoding='UTF-8', newline='') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(['Host','DNS','Issuer','Signature_Algorithm','Signature_OID_Algorithm','Cert_Creation_Date','Cert_EOL','Date_Difference','Tested_Date'])
+            writer.writerow(['Host','DNS','Issuer','Subject','Signature_Algorithm','Cert_Creation_Date','Cert_EOL','Date_Difference','Tested_Date'])
             for Target in Dict_Result['Certificate']:
                 Array_Temp = []
                 Array_Temp.append(Target)
