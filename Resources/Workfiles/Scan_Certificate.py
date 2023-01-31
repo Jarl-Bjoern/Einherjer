a490299dc705cf74802f20793643a7889c73ba6d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-01-31 17:48:29 +0100

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -7,7 +7,7 @@ from Resources.Header_Files.Variables import *
 from Resources.Standard_Operations.Logs import Logs
 from Resources.Colors import Colors
 
-def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_context(), Dict_Temp = {'DNS': "", 'Issuer': "", 'Subject': "", 'Signature_Algorithm': "",'Cert_Creation_Date': "", 'Cert_EOL': "", 'Date_Difference': "", 'Current_Date': ""}):
+def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_context(), Dict_Temp = {'DNS': "", 'Issuer': "", 'Subject': "", 'Signature_Algorithm': "", 'Cert_Creation_Date': "", 'Cert_EOL': "", 'Date_Difference': "", 'Current_Date': ""}):
     if ('https://' in url): URL = url.split('https://')[1]
     elif ('http://' in url): URL = url.split('http://')[1]
     else: URL = url
