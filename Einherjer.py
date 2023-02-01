5767bce1f2401b0a58f9288bdf316e3cfb1f65a9
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-01 17:41:50 +0100

    Update Einherjer.py

--- Einherjer.py
+++ Einherjer.py
@@ -74,14 +74,6 @@ def main(Date, args, Dict_Result = {'Certificate': {}, 'Fuzzing': {}, 'Header': 
         try: makedirs(Screen_Dir)
         except FileExistsError: pass
 
-    # Template_Filtering
-    if (args.read_config_cookie_security_flags != None): Array_Security_Flags = Standard.Read_Template(join(dirname(realpath(__file__)), "Templates/http_cookie_security.txt"))
-    if (args.read_config_http_header != None): Array_Header, Array_HTTP_Filter = Standard.Read_File_Special(join(dirname(realpath(__file__)), "Templates/http_header.txt"))
-    if (args.read_config_http_header_api != None): Array_Header, Array_HTTP_Filter = Standard.Read_File_Special(join(dirname(realpath(__file__)), "Templates/http_header_api.txt"))
-    if (args.read_config_http_methods != None): Array_HTTP_Methods = Standard.Read_Template(join(dirname(realpath(__file__)), "Templates/http_methods.txt"))
-    if (args.read_config_ssh_ciphers != None): Array_SSH_Algorithms = Standard.Read_Template(join(dirname(realpath(__file__)), "Templates/ssh_ciphers.txt"))
-    if (args.read_config_ssl_ciphers != None): Array_TLS_Algorithms = Standard.Read_Template(join(dirname(realpath(__file__)), "Templates/ssl_ciphers.txt"))
-
     # Proxy_Settings
     if (args.add_http_proxy != None): Dict_Proxies['http'] = args.add_http_proxy
     if (args.add_https_proxy != None): Dict_Proxies['https'] = args.add_https_proxy
