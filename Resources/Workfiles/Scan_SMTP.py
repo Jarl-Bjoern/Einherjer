4e1ce0133d2af01805cec872011a622b8154a8fe
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-11 17:29:22 +0100

    Update Scan_SMTP.py

--- Resources/Workfiles/Scan_SMTP.py
+++ Resources/Workfiles/Scan_SMTP.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
 
+class Check_SMTP:
+    def Check_TLS():
+        pass
+
+    def Check_Open_Relay(Target, sender, receivers, message):
+        Mail = smtplib.SMTP(Target,25)
+        Mail.sendmail(sender, receivers, message)         
+        Mail.quit()
