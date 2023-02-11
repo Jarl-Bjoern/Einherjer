65a104feea5e35b235dd6cc62684cce74634b3c9
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-11 19:31:58 +0100

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -26,7 +26,7 @@ def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_con
         with create_connection((URL, int(Port)), timeout=t_seconds) as sock:
             with context.wrap_socket(sock, server_hostname=URL) as ssock:
                 cert_der = ssock.getpeercert(True)
-                cert = x509.load_der_x509_certificate(cert_der, default_backend())
+                cert = load_der_x509_certificate(cert_der, default_backend())
                 cert_key = X509.load_cert_string(cert_der, X509.FORMAT_DER)
 
                 Current_Date = datetime.now()
