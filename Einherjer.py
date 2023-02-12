1e2508dc70d5017b4240460a5e72c30ace6c1938
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 13:52:39 +0100

    Update Einherjer.py

--- Einherjer.py
+++ Einherjer.py
@@ -5,10 +5,11 @@
 # Libraries
 from os import name as osname
 from os.path import dirname, join, realpath
+from Resources.Header_Files.Libraries import *
 from sys import argv
 from subprocess import call
 
 # Main
 if __name__ == '__main__':
-    if (osname == 'nt'): call(['powershell',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.ps1")}',f'{argv[1:]}'])
-    else: call(['sudo','bash',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.sh")}',f'{argv[1:]}'])
+    if (osname == 'nt'): call(['powershell',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.ps1")}',f'{args}'])
+    else: call(['sudo','bash',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.sh")}',f'{args}'])
