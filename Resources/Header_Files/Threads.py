70bf187a0571ad7c7ba2bb64c63ad783f6ebcf64
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 18:41:23 +0100

    Update Threads.py

--- Resources/Header_Files/Threads.py
+++ Resources/Header_Files/Threads.py
@@ -11,25 +11,25 @@ from ..Workfiles.Scan_Header import Check_Site_Header
 from ..Workfiles.Scan_Screen import Take_Screenshot
 
 # Functions
-def Thread_Scanning_Start(url, t_seconds, queue, driver_options, scan_ssl, scan_header, scan_fuzzing, scan_ssh, scan_fuzzing_recurse, scan_security_flag, scan_certificate, screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout, Host_Name = "", Target = ""):
+def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout, Host_Name = "", Target = ""):
     try:
         Dict_Result = queue.get()
         Host_Name = Get_Host_Name(url)
-        if (driver_options != None and '//' in url and 'http' in url):
-            Take_Screenshot(url, driver_options, screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout)
-        if (scan_header != False and '//' in url and 'http' in url):
+        if (dict_switch['scan_screenshot'] != None and '//' in url and 'http' in url):
+            Take_Screenshot(url, dict_switch['scan_screenshot'], screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout)
+        if (dict_switch['scan_header'] != False and '//' in url and 'http' in url):
             Dict_Result['Header'][html_decode(url)], Dict_Result['Information'][html_decode(url)] = Check_Site_Header(url, t_seconds, Host_Name)
-        if (scan_ssl != False and '//' in url and 'https' in url):
+        if (dict_switch['scan_ssl'] != False and '//' in url and 'https' in url):
             Dict_Result['SSL'][html_decode(url)] = SSL_Vulns(url, t_seconds)
-        if (scan_security_flag != False and '//' in url and 'http' in url):
+        if (dict_switch['scan_security_flags'] != False and '//' in url and 'http' in url):
             Dict_Result['Security_Flag'][html_decode(url)] = Check_Security_Flags(url, t_seconds, Host_Name)
-        if (scan_certificate != False and '//' in url and 'https' in url):
+        if (dict_switch['scan_certificate'] != False and '//' in url and 'https' in url):
             Dict_Result['Certificate'][html_decode(url)] = Check_Certificate(url, t_seconds, Host_Name)
-        if (scan_fuzzing != False and '//' in url and 'http' in url):
+        if (dict_switch['scan_fuzzing'] != False and '//' in url and 'http' in url):
             Dict_Result['Fuzzing'][html_decode(url)] = Check_Site_Paths(url, t_seconds)
-        if (scan_fuzzing_recurse != False and '//' in url and 'http' in url):
-            pass
-        if (scan_ssh != False and '//' in url and 'ssh' in url):
+#        if (scan_fuzzing_recurse != False and '//' in url and 'http' in url):
+#            pass
+        if (dict_switch['scan_ssh'] != False and '//' in url and 'ssh' in url):
             try:
                 if (':' not in url): Dict_Result['SSH'][html_decode(url)] = SSH_Vulns((url, 22))
                 else:
