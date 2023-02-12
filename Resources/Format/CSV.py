0ad8f2b4f16cc6e45087b53988972dc20b9e5b4a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 12:24:17 +0100

    Update CSV.py

--- Resources/Format/CSV.py
+++ Resources/Format/CSV.py
@@ -3,7 +3,7 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Header_Files.Variables import *
+from ..Header_Files.Variables import *
 
 def CSV_Table(Dict_Result, location, Array_Files = []):
     try: import csv
