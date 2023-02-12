83382875b43bb684fa27c0ca8a2d132b2479a68e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 12:07:54 +0100

    Update Standard.py

--- Resources/Standard_Operations/Standard.py
+++ Resources/Standard_Operations/Standard.py
@@ -3,9 +3,9 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Standard_Operations.Colors import Colors
-from Header_Files.Variables import *
-from Standard_Operations.Logs import *
+from Resources.Standard_Operations.Colors import Colors
+from Resources.Header_Files.Variables import *
+from Resources.Standard_Operations.Logs import *
 
 class Standard:
     def Stdout_Output(Text_Array, Output_Seconds):
