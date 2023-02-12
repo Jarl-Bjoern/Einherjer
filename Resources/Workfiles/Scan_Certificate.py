d137abc0d4f501b22e6a60239ea674cbbe57301f
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 11:46:41 +0100

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -3,9 +3,9 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Resources.Header_Files.Variables import *
-from Resources.Standard_Operations.Logs import Logs
-from Resources.Standard_Operations.Colors import Colors
+from Header_Files.Variables import *
+from Standard_Operations.Logs import Logs
+from Standard_Operations.Colors import Colors
 
 def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_context(), Dict_Temp = {'DNS': "", 'Issuer': "", 'Subject': "", 'Signature_Algorithm': "", 'Cert_Creation_Date': "", 'Cert_EOL': "", 'Date_Difference': "", 'Current_Date': ""}):
     if ('https://' in url): URL = url.split('https://')[1]
