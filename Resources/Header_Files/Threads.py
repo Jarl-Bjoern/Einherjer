ac676ddf0f7bf1daea848afaae24870d1c842b96
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 18:52:58 +0100

    Update Threads.py

--- Resources/Header_Files/Threads.py
+++ Resources/Header_Files/Threads.py
@@ -15,20 +15,32 @@ def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch
     try:
         Dict_Result = queue.get()
         Host_Name = Get_Host_Name(url)
-        if (dict_switch['scan_screenshot'] != None and '//' in url and 'http' in url):
-            Take_Screenshot(url, dict_switch['scan_screenshot'], screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout)
-        if (dict_switch['scan_header'] != False and '//' in url and 'http' in url):
-            Dict_Result['Header'][html_decode(url)], Dict_Result['Information'][html_decode(url)] = Check_Site_Header(url, t_seconds, Host_Name)
-        if (dict_switch['scan_ssl'] != False and '//' in url and 'https' in url):
-            Dict_Result['SSL'][html_decode(url)] = SSL_Vulns(url, t_seconds)
-        if (dict_switch['scan_security_flags'] != False and '//' in url and 'http' in url):
-            Dict_Result['Security_Flag'][html_decode(url)] = Check_Security_Flags(url, t_seconds, Host_Name)
+
+        # Certificat
         if (dict_switch['scan_certificate'] != False and '//' in url and 'https' in url):
             Dict_Result['Certificate'][html_decode(url)] = Check_Certificate(url, t_seconds, Host_Name)
+
+        # Fuzzing
         if (dict_switch['scan_fuzzing'] != False and '//' in url and 'http' in url):
             Dict_Result['Fuzzing'][html_decode(url)] = Check_Site_Paths(url, t_seconds)
+
+        # Recursive_Fuzzing
 #        if (scan_fuzzing_recurse != False and '//' in url and 'http' in url):
 #            pass
+
+        # Header
+        if (dict_switch['scan_header'] != False and '//' in url and 'http' in url):
+            Dict_Result['Header'][html_decode(url)], Dict_Result['Information'][html_decode(url)] = Check_Site_Header(url, t_seconds, Host_Name)
+
+        # Screenshot
+        if (dict_switch['scan_screenshot'] != None and '//' in url and 'http' in url):
+            Take_Screenshot(url, dict_switch['scan_screenshot'], screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout)
+
+        # Security_Flags
+        if (dict_switch['scan_security_flags'] != False and '//' in url and 'http' in url):
+            Dict_Result['Security_Flag'][html_decode(url)] = Check_Security_Flags(url, t_seconds, Host_Name)
+
+        # SSH
         if (dict_switch['scan_ssh'] != False and '//' in url and 'ssh' in url):
             try:
                 if (':' not in url): Dict_Result['SSH'][html_decode(url)] = SSH_Vulns((url, 22))
@@ -36,6 +48,11 @@ def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch
                     Target = url.split(':')
                     Dict_Result['SSH'][html_decode(url)] = SSH_Vulns((Target[0]), int(Target[1]))
             except SSHException: Logs.Write_Log(html_decode(url), Host_Name)
+
+        # SSL
+        if (dict_switch['scan_ssl'] != False and '//' in url and 'https' in url):
+            Dict_Result['SSL'][html_decode(url)] = SSL_Vulns(url, t_seconds)
+
     except (ConnectionError, gaierror, WebDriverException, RequestException): Logs.Write_Log(html_decode(url), Host_Name)
     finally:
         queue.put(Dict_Result)
