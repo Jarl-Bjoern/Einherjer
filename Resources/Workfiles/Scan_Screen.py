dfe0dbb18daf6dc0d7762ffea036c301ac380bae
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 12:27:29 +0100

    Update Scan_Screen.py

--- Resources/Workfiles/Scan_Screen.py
+++ Resources/Workfiles/Scan_Screen.py
@@ -3,9 +3,9 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Resources.Header_Files.Variables import *
-from Resources.Standard_Operations.Logs import Logs
-from Resources.Standard_Operations.Colors import Colors
+from ..Header_Files.Variables import *
+from ..Standard_Operations.Logs import Logs
+from ..Standard_Operations.Colors import Colors
 
 class Web:
     def Driver_Specification(options, Chrome_Driver_Location = ""):
