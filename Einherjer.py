5b0daf4888b3b0955df619aac18e3e605820616e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 10:57:38 +0100

    Update Einherjer.py

--- Einherjer.py
+++ Einherjer.py
@@ -8,4 +8,4 @@ from Resources.Header_Files.Threads import *
 # Main
 if __name__ == '__main__':
     if (osname == 'nt'): system(f'powershell {join(dirname(realpath(__file__)), "Resources/Start_Files/start.ps1")} {args}')
-    else: system(f'sudo python3 {join(dirname(realpath(__file__)), "Resources/Start_Files/start.sh")} {args}')
+    else: system(f'sudo bash {join(dirname(realpath(__file__)), "Resources/Start_Files/start.sh")} {args}')
