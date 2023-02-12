eabe0fbc72b82ed6638868ae9b1deb8b3ba723b4
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 12:35:55 +0100

    Update ArgParser.py

--- Resources/Header_Files/ArgParser.py
+++ Resources/Header_Files/ArgParser.py
@@ -9,7 +9,7 @@ SYSTEM_PATH.append(abspath("../Standard_Operations"))
 SYSTEM_PATH.append(".")
 
 # Libraries
-from Colors import Colors
+from ..Standard_Operations.Colors import Colors
 from .Libraries import dirname, join, realpath
 
 # Functions
