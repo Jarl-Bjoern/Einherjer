e1051f9100f9ce3f129b715ca1785c8b5de2d017
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 11:27:32 +0100

    Update start.sh

--- Resources/Start_Files/start.sh
+++ Resources/Start_Files/start.sh
@@ -2,9 +2,8 @@
 # Rainer Christian Bjoern Herold
 TEMP_PATH=$(readlink -f -- "$0")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
-FULL_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-21}
-echo $TEMP_PATH
-echo $SCRIPT_NAME
+FULL_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-19}
+echo $FULL_PATH
 source venv/bin/activate
 sudo python3 "../main.py" "$@"
 deactivate
