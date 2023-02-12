c7678df56ad000dc86523046e767b6a38e2ab77c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 11:37:59 +0100

    Update start.sh

--- Resources/Start_Files/start.sh
+++ Resources/Start_Files/start.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 # Rainer Christian Bjoern Herold
+
+# Variables
 TEMP_PATH=$(readlink -f -- "$0")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-13}
 BASE_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-23}
+
+# Color
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+ORANGE='\033[1;33m'
+NORANGE='\033[0;33m'
+PURPLE='\033[0;35m'
+UNDERLINE='\033[0;4m'
+NOCOLOR='\033[0m'
+
+# Main
 if [[ -d "$BASE_PATH/venv" ]]; then
         source "$BASE_PATH/"venv/bin/activate
         sudo python3 "$SCRIPT_PATH/main.py" "$@"
         deactivate
 else
-        echo -e "Please use a virtual environment!\n\nvirtualenv $BASE_PATH/venv\nsource $BASE_PATHvenv/bin/activate\npip3 install -r Setup/requirements.txt\ndeactivate"
+        echo -e "{CYAN}Please use a virtual environment!{NOCOLOR}\n\n{ORANGE}virtualenv $BASE_PATH/venv\nsource $BASE_PATH/venv/bin/activate\npip3 install -r Setup/requirements.txt\ndeactivate{NOCOLOR}"
 fi
