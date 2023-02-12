cea091fa65f520a170040ea2343017c0a76e860a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 11:42:43 +0100

    Update main.py

--- Resources/main.py
+++ Resources/main.py
@@ -3,8 +3,8 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Resources.Header_Files.Threads import *
-from Resources.Workfiles.Scan_Screen import Web
+from Header_Files.Threads import *
+from Workfiles.Scan_Screen import Web
 
 # Main_Function
 def main(Date, args, Dict_Result = {'Certificate': {}, 'Fuzzing': {}, 'Header': {}, 'Information': {}, 'Security_Flag': {}, 'SSH': {}, 'SSL': {}}, Dict_Proxies = {'http': '', 'https': ''}, Array_HTTP_Filter = [], Array_Switch = [], Array_Thread_Args = [], Dict_Switch = {}, Dict_Threads = {}, Counter_Connections = 0, Switch_Internet_Connection = False, Screen_Dir = "", driver_options = None):
