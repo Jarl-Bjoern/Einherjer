bb59799755198b4a5356d5daa3942089c75b3324
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-12 13:18:46 +0100

    Update Einherjer.py

--- Einherjer.py
+++ Einherjer.py
@@ -10,5 +10,5 @@ from subprocess import run
 
 # Main
 if __name__ == '__main__':
-    if (osname == 'nt'): run(['powershell',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.ps1")}',argv])
-    else: run(['sudo','bash',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.sh")}',argv])
+    if (osname == 'nt'): run(['powershell',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.ps1")}',f'{argv}')])
+    else: run(['sudo','bash',f'{join(dirname(realpath(__file__)), "Resources/Start_Files/start.sh")}',f'{argv}'])
