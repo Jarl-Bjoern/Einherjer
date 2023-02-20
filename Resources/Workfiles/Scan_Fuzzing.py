11d6e880e810d840d429d9c0d838494a32513ebd
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-20 10:22:49 +0100

    Update Scan_Fuzzing.py

--- Resources/Workfiles/Scan_Fuzzing.py
+++ Resources/Workfiles/Scan_Fuzzing.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
+
+def Check_Site_Paths(url, t_seconds, array_wordlists, Array_Temp = [], Array_Status_Code = ["200", "302", "405", "500"]):
+    for Word in array_wordlists:
+        URL = f'{url}/{Word}'
+        r = get(URL, timeout=t_seconds, verify=False, allow_redirects=True)
+        if (str(r.status_code) in Array_Status_Code):
+            if (URL not in Array_Temp): Array_Temp.append(URL)
+        sleep(t_seconds)
+
+    return Array_Temp
