5d9aaea2b58f5978f49b4e2449bfd7ed3e08fa00
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-20 21:18:35 +0100

    Update Scan_Service.py

--- Resources/Workfiles/Scan_Service.py
+++ Resources/Workfiles/Scan_Service.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
+
+def Port_Scan(addr, port):
+    socket_defaulttimeout(1)
+    with (socket(AF_INET, SOCK_STREAM)) as s:
+        result = s.connect_ex((addr, port))
+
+    return port
+
+def Banner_Grabbing(addr, port):
+    socket_defaulttimeout(1)
+
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((addr, port))
+        try:
+           return s.recv(1024).decode()
+        except TimeoutError:
+           return "It was not possible to collect any kind of data"
