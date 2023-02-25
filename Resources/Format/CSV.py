3090a57eaa809e1db90420d81963a99b2f3c3435
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-25 13:17:43 +0100

    Update CSV.py

--- Resources/Format/CSV.py
+++ Resources/Format/CSV.py
@@ -124,7 +124,7 @@ def CSV_Table(Dict_Result, location, Array_Files = []):
                         for _ in Result_Right:
                             if (_['Protocol'] != "" and _['Ciphers'] != []):
                                 for Cipher in _['Ciphers']:
-                                    Temp_Arr = [_['Protocol'],Cipher['Ciphers'],Cipher['Key_Size'],Cipher['Anonymous']]
+                                    Temp_Arr = [_['Protocol'],Cipher['Cipher'],Cipher['Key_Size'],Cipher['Anonymous']]
                                     if (Cipher['Curve_Name'] != None and Cipher['Curve_Name'] != ''):
                                         Temp_Arr.append('-')
                                     else: Temp_Arr.append(Cipher['Curve_Name'])
