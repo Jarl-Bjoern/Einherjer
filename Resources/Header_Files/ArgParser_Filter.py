ccf6175cbfa64e68bcabd0993e5ac068e61a9750
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-02-25 16:47:56 +0100

    Create ArgParser_Filter.py

---
+++ Resources/Header_Files/ArgParser_Filter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Libraries
+from Resources.Standard_Operations.Colors import Colors
+from .Libraries import dirname, join, realpath
+
+# Functions
+def Argument_Parser(Template_Location = dirname(realpath(__file__)).replace('Resources/Header_Files', 'Templates')):
+    from argparse import ArgumentParser, RawTextHelpFormatter, SUPPRESS
+
+    Program_Description = """-------------------------------------------------------------------------------------
+|  Rainer Christian Bjoern Herold                                                   |
+|  Copyright 2022-2023. All rights reserved.                                        |
+|                                                                                   |
+|  Please do not use the program for illegal activities.                            |
+|                                                                                   |
+|  If you got any problems don't hesitate to contact me so I can try to fix them.   |
+|                                                                                   |
+|  If you use the "Kali-Last-Snapshot" repository, you might install a slightly     |
+|  older driver of Chromium with the command "apt install -y chromium". If this     |
+|  is the case, then you should check after the installation with the command       |
+|  "apt-cache policy chromium" which version was installed and then download the    |
+|  appropriate Chrome Webdriver from the following page                             |
+|  "https://chromedriver.chromium.org/downloads" and replace it instead.            |
+|                                                                                   |
+|  In some cases it can happen that after an installation of Chromium, the program  |
+|  cannot create processes, because no environment variable for the Chromedriver    |
+|  can be accessed, therefore the system must be restarted once.                    |
+-------------------------------------------------------------------------------------
+"""
+
+    parser = ArgumentParser(add_help=False, formatter_class=RawTextHelpFormatter, description=Colors.ORANGE+Program_Description+Colors.RESET, allow_abbrev=False)
+    debug_arguments = parser.add_argument_group(Colors.ORANGE+'debug arguments'+Colors.RESET)
+    format_arguments = parser.add_argument_group(Colors.ORANGE+'format arguments'+Colors.RESET)
+    optional = parser.add_argument_group(Colors.ORANGE+'optional arguments'+Colors.RESET)
+
+    debug_arguments.add_argument('-d', '--debug', type=bool, nargs='?', default=False, help=Colors.GREEN+'This Parameter deactivates the terminal clearing after starting the tool.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+
+    optional.add_argument('-h','--help', action='help', default=SUPPRESS, help=Colors.GREEN+'Show this help message and exit.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+
+    del ArgumentParser, RawTextHelpFormatter, SUPPRESS
+    return parser.parse_args()
