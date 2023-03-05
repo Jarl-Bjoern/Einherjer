31c88096a49d99d6a41e4d62fb55904dce077b0e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 19:05:15 +0100

    Update CSV.py

--- Resources/Format/CSV.py
+++ Resources/Format/CSV.py
@@ -118,10 +118,10 @@ def CSV_Table(Dict_Result, location, Array_Files = []):
                 Array_Temp.append(Target)
                 for Result_Left, Result_Right in Dict_Result['HTTP_Methods'][Target].items():
                     if (Result_Left == "DNS" and Result_Right == ""):  Result_Right = "FEHLT"
-                    elif (Result_Left != "DNS" and Result_Right == ""): Result_Right = "FEHLT"
+                    elif (Result_Left != "DNS" and Result_Right == "FALSE"): Result_Right = "FEHLT"
 
-                    if (Result_Left != "DNS" and Result_Right != "FALSE"): Array_Temp.append("✓")
-                    elif (Result_Left == "DNS" and Result_Right != "FALSE"): Array_Temp.append(Result_Right)
+                    if (Result_Left != "DNS" and Result_Right != "FEHLT"): Array_Temp.append("✓")
+                    elif (Result_Left == "DNS" and Result_Right != "FEHLT"): Array_Temp.append(Result_Right)
                     elif (Result_Left == "DNS" and Result_Right == "FEHLT"): Array_Temp.append("-")
                     else: Array_Temp.append("X")
                 writer.writerow(Array_Temp)           
