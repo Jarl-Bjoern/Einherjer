b3e3f0e8cedab0432820de79de5bd8d4f2436943
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 18:36:54 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -7,16 +7,16 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}, Switch_URL = False):
+def Check_HTTP_Methods(url, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}, Switch_URL = False):
     async def Check_Methods():
         async with ClientSession() as s:
             for Method in Array_HTTP_Methods:
                 try:
                     async with s.request(Method, url) as r:
                         if (str(r.status) == "200"):
-                            print (Method)
+                            Dict_Temp[Method] = "True"
                 except ServerDisconnectedError:
-                    pass
+                    Dict_Temp[Method] = "False"
 
     asyncio.run(Check_Methods())
 
