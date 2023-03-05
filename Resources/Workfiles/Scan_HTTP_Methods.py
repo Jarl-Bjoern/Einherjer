e7beb8c38479c268c147ef849a0e0c3e1629d438
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:11:37 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -9,6 +9,8 @@ from ..Standard_Operations.Colors import Colors
 
 def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}):
     r = HTTPSConnection(url)
+    Dict_Temp['DNS'] = Host_Name
+
     for Method in Array_HTTP_Methods:
         r.request(Method, "/")
         res = r.getresponse()
