933d881ac1d4ae9f9d64891f0d445216203d071d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:52:27 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -8,8 +8,8 @@ from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
 def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}, Switch_URL = False):
+    from requests import request
     for Method in Array_HTTP_Methods:
-        print (Method)
         try:
             r = request(Method, url, timeout=(5,5))
             if ("200" in str(res)):
