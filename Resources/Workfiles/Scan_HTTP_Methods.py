58e3b17db3bd18135447d23fc12ed14568ef6752
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:18:18 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -7,8 +7,10 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}):
-    if ('https://' in url): URL = url.split('https://')[1]
+def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}, Switch_URL = False):
+    if ('https://' in url):
+        URL = url.split('https://')[1]
+        Switch_URL = True
     elif ('http://' in url): URL = url.split('http://')[1]
     else: URL = url
 
@@ -22,15 +24,18 @@ def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNE
     if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
     else: Dict_Temp['DNS'] = ""
 
-    try:
-        r = HTTPSConnection(URL, port=Port)
-        for Method in Array_HTTP_Methods:
-            r.request(Method, "/")
-            res = r.getresponse()
-            if (res.status == "200" and res.reason == "OK"):
-                print(res.status, res.reason)
-            sleep(0.25)
-    except TimeoutError:
-        pass
+    if (Switch_URL = True):
+        try: r = HTTPSConnection(URL, port=Port, timeout=15)
+        except TimeoutError: pass
+    else:
+        try: r = HTTPConnection(URL, port=Port, timeout=15)
+        except TimeoutError: pass        
+    
+    for Method in Array_HTTP_Methods:
+        r.request(Method, "/")
+        res = r.getresponse()
+        if (res.status == "200" and res.reason == "OK"):
+            print(res.status, res.reason)
+        sleep(0.25)
 
     return Dict_Temp
