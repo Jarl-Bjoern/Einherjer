fe66533f64efb7a39bbdd392e5da62a98370fc49
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:12:33 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -8,8 +8,21 @@ from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
 def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}):
-    r = HTTPSConnection(url)
-    Dict_Temp['DNS'] = Host_Name
+    if ('https://' in url): URL = url.split('https://')[1]
+    elif ('http://' in url): URL = url.split('http://')[1]
+    else: URL = url
+
+    if ('/' in URL):
+        Temp = URL.split('/')[0]
+        URL = Temp
+
+    if (url.count(':') > 1): Port = url.split(':')[2]
+    else: Port = 443
+
+    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
+    else: Dict_Temp['DNS'] = ""
+
+    r = HTTPSConnection(URL)
 
     for Method in Array_HTTP_Methods:
         r.request(Method, "/")
