9e617f3884db806994252d48cf69f57eb145453e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 18:31:53 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -13,7 +13,7 @@ def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNE
             for Method in Array_HTTP_Methods:
                 try:
                     async with s.request(Method, url) as r:
-                        Output = await r.status
+                        Output = await str(r.status)
                         print (Output)
                 except ServerDisconnectedError:
                     pass
