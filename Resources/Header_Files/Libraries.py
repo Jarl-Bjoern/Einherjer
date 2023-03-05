5479049fe24b3455d2d2573c24d695cab1c3ef78
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:15:37 +0100

    Update Libraries.py

--- Resources/Header_Files/Libraries.py
+++ Resources/Header_Files/Libraries.py
@@ -102,7 +102,7 @@ try:
                         from dns.query import xfr
                         from dns.zone import from_xfr
                         from ftplib import FTP
-                        from http.client import HTTPSConnection
+                        from http.client import HTTPConnection, HTTPSConnection
                         from json import loads as json_loads
                         from os import environ, rename
                         from requests import get, Session
@@ -152,7 +152,7 @@ try:
                 if (args.scan_security_flags != False):
                     from requests import Session
                 if (args.scan_site_http_methods != False):
-                    from http.client import HTTPSConnection
+                    from http.client import HTTPConnection, HTTPSConnection
                 if (args.scan_smtp != False):
                     from smtplib import SMTP
         elif (argv[1] == '-h'):
