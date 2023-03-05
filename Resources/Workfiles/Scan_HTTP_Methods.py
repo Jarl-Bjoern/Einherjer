f0e9a2bd2d390666479b05f0cd8a3ef6f6ec3243
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:24:11 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -8,36 +8,52 @@ from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
 def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'DELETE': "", 'HEAD': "", 'OPTIONS': "", 'PATCH': "", 'POST': "", 'PUT': "", 'TRACE': ""}, Switch_URL = False):
-    if ('https://' in url):
-        URL = url.split('https://')[1]
-        Switch_URL = True
-    elif ('http://' in url): URL = url.split('http://')[1]
-    else: URL = url
+    import requests
 
-    if ('/' in URL):
-        Temp = URL.split('/')[0]
-        URL = Temp
-
-    if (url.count(':') > 1): Port = url.split(':')[2]
-    else: Port = 443
-
-    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
-    else: Dict_Temp['DNS'] = ""
-
-    if (Switch_URL == True):
-        try: r = HTTPSConnection(URL, port=Port, timeout=15)
-        except TimeoutError: pass
-    else:
-        try: r = HTTPConnection(URL, port=Port, timeout=15)
-        except TimeoutError: pass        
-    
     for Method in Array_HTTP_Methods:
         try:
-            r.request(Method, "/")
-            res = r.getresponse()
+            req = requests.Request(Method, url)
+            r = req.prepare()
+            s = requests.Session()
+            s.send(r)
+            res = s.getresponse()
             if (res.status == "200" and res.reason == "OK"):
                 print(res.status, res.reason)
             sleep(0.25)
         except TimeoutError: pass
+
+#
+#
+#    if ('https://' in url):
+#        URL = url.split('https://')[1]
+#        Switch_URL = True
+#    elif ('http://' in url): URL = url.split('http://')[1]
+#    else: URL = url
+#
+#    if ('/' in URL):
+#        Temp = URL.split('/')[0]
+#        URL = Temp
+#
+#    if (url.count(':') > 1): Port = url.split(':')[2]
+#    else: Port = 443
+#
+#    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
+#    else: Dict_Temp['DNS'] = ""
+#
+#    if (Switch_URL == True):
+#        try: r = HTTPSConnection(URL, port=Port, timeout=15)
+#        except TimeoutError: pass
+#    else:
+#        try: r = HTTPConnection(URL, port=Port, timeout=15)
+#        except TimeoutError: pass        
+#    
+#    for Method in Array_HTTP_Methods:
+#        try:
+#            r.request(Method, "/")
+#            res = r.getresponse()
+#            if (res.status == "200" and res.reason == "OK"):
+#                print(res.status, res.reason)
+#            sleep(0.25)
+#        except TimeoutError: pass
 
     return Dict_Temp
