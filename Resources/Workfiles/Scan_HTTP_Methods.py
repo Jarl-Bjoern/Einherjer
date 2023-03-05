851ebb88fb9e82e5f9bdb5610f45cca511c1572b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 18:33:30 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -20,38 +20,4 @@ def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNE
 
     asyncio.run(Check_Methods())
 
-#
-#
-#    if ('https://' in url):
-#        URL = url.split('https://')[1]
-#        Switch_URL = True
-#    elif ('http://' in url): URL = url.split('http://')[1]
-#    else: URL = url
-#
-#    if ('/' in URL):
-#        Temp = URL.split('/')[0]
-#        URL = Temp
-#
-#    if (url.count(':') > 1): Port = url.split(':')[2]
-#    else: Port = 443
-#
-#    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
-#    else: Dict_Temp['DNS'] = ""
-#
-#    if (Switch_URL == True):
-#        try: r = HTTPSConnection(URL, port=Port, timeout=15)
-#        except TimeoutError: pass
-#    else:
-#        try: r = HTTPConnection(URL, port=Port, timeout=15)
-#        except TimeoutError: pass        
-#    
-#    for Method in Array_HTTP_Methods:
-#        try:
-#            r.request(Method, "/")
-#            res = r.getresponse()
-#            if (res.status == "200" and res.reason == "OK"):
-#                print(res.status, res.reason)
-#            sleep(0.25)
-#        except TimeoutError: pass
-
     return Dict_Temp
