893a40cd1ab1e129065f6244471d69e2af8df154
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-05 17:14:43 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -22,13 +22,15 @@ def Check_HTTP_Methods(url, t_seconds, Host_Name, Dict_Temp = {'DNS': "", 'CONNE
     if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
     else: Dict_Temp['DNS'] = ""
 
-    r = HTTPSConnection(URL)
-
-    for Method in Array_HTTP_Methods:
-        r.request(Method, "/")
-        res = r.getresponse()
-        if (res.status == "200" and res.reason == "OK"):
-            print(res.status, res.reason)
-        sleep(0.25)
+    try:
+        r = HTTPSConnection(URL, port=Port)
+        for Method in Array_HTTP_Methods:
+            r.request(Method, "/")
+            res = r.getresponse()
+            if (res.status == "200" and res.reason == "OK"):
+                print(res.status, res.reason)
+            sleep(0.25)
+    except TimeoutError:
+        pass
 
     return Dict_Temp
