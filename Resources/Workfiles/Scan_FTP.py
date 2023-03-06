5fce1e0430a24ac260c1a5c76c2f91971817fc4d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 19:01:44 +0100

    Update Scan_FTP.py

--- Resources/Workfiles/Scan_FTP.py
+++ Resources/Workfiles/Scan_FTP.py
@@ -3,11 +3,18 @@
 # Rainer Christian Bjoern Herold
 
 def Check_FTP(url, Dict_Temp = {'DNS': "", 'Anonymous_Login': ""]):
+    if ('ftp://' in url): URL = url.split('ftp://')[1]
+    else: URL = url
+
+    if (url.count(':') > 1): Port = url.split(':')[2]
+    else: Port = 21
+
+    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
+    else: Dict_Temp['DNS'] = ""
+
     ftp = FTP()
-    Host = "192.168.198.129"
-    Port = 21
     try:
-        ftp.connect(HOST, PORT)
+        ftp.connect(URL, Port)
         msg = ftp.login()
         Banner = ftp.getwelcome()
         if ("Login successful." in msg):
