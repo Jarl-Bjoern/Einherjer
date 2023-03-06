908e3c7731bbeac61956baaf9db189aaf041c83e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 17:37:28 +0100

    Update Methods.py

--- Resources/Filter/Methods.py
+++ Resources/Filter/Methods.py
@@ -21,10 +21,11 @@ class Filter:
                 Dict_DNS[str(x[0])] = str(x[1])
 
             with open(join(Output_Location, 'hostnames.txt'), 'w') as f:
-                for _ in Target_Array:
-                    if (_ in Dict_DNS): f.write(f'{Dict_DNS[_]}\n')
-                    else: f.write('-\n')
-            Array_Temp.append(join(Output_Location, 'hostnames.txt'))
+                with open(join(Output_Location, 'affected_systems.txt'), 'w') as af:
+                    for _ in Target_Array:
+                        if (_ in Dict_DNS): f.write(f'{Dict_DNS[_]}\n')
+                        else: f.write('-\n')
+            Array_Temp.append(join(Output_Location, 'hostnames.txt')), Array_Temp.append(join(Output_Location, 'affected_systems.txt'))
         except FileNotFoundError:
             pass
 
