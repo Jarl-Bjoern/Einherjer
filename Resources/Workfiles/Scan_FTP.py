d193133ed6c57a8b08cca56178086147b18a6b64
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 19:04:46 +0100

    Update Scan_FTP.py

--- Resources/Workfiles/Scan_FTP.py
+++ Resources/Workfiles/Scan_FTP.py
@@ -2,23 +2,30 @@
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
 
-def Check_FTP(url, Dict_Temp = {'DNS': "", 'Anonymous_Login': ""]):
-    if ('ftp://' in url): URL = url.split('ftp://')[1]
-    else: URL = url
+class Check_FTP:
+    def FTP_Anonymous_Check(url, Dict_Temp = {'DNS': "", 'Anonymous_Login': ""]):
+        if ('ftp://' in url): URL = url.split('ftp://')[1]
+        else: URL = url
 
-    if (url.count(':') > 1): Port = url.split(':')[2]
-    else: Port = 21
+        if (url.count(':') > 1): Port = url.split(':')[2]
+        else: Port = 21
 
-    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
-    else: Dict_Temp['DNS'] = ""
+        if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
+        else: Dict_Temp['DNS'] = ""
 
-    ftp = FTP()
-    try:
-        ftp.connect(URL, Port)
-        msg = ftp.login()
-        Banner = ftp.getwelcome()
-        if ("Login successful." in msg):
-            Dict_Temp['Anonymous_Login'] = "True"
-    except ConnectionRefusedError: pass
+        ftp = FTP()
+        try:
+            ftp.connect(URL, Port)
+            msg = ftp.login()
+            Banner = ftp.getwelcome()
+            if ("Login successful." in msg):
+                Dict_Temp['Anonymous_Login'] = "True"
+        except ConnectionRefusedError: pass
 
-    return Dict_Temp
+        return Dict_Temp
+
+    def FTP_Brute_Force(url):
+        pass
+
+    def FTP_Password_Spray(url):
+        pass
