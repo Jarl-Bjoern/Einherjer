1a3eaad2fc4c70c27687a0984523eb33e5a7f557
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 19:40:10 +0100

    Update Scan_Fuzzing.py

--- Resources/Workfiles/Scan_Fuzzing.py
+++ Resources/Workfiles/Scan_Fuzzing.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Site_Paths(url, t_seconds, array_wordlists, Array_Temp = [], Array_Status_Code = ["200", "204", "301", "302", "307", "308", "401", "403", "405", "500"]):
+def Check_Site_Paths(url, t_seconds, array_wordlists, Dict_Result = {"200": [], "204": [], "301": [], "302": [], "307": [], "308": [], "401": [], "403": [], "405": [], "500": []}, Array_Temp = [], Array_Status_Code = ["200", "204", "301", "302", "307", "308", "401", "403", "405", "500"]):
     async def Check_Fuzz():
         Limit = TCPConnector(limit_per_host=100)
         async with ClientSession(connector=Limit, trust_env=True) as s:
@@ -15,7 +15,9 @@ def Check_Site_Paths(url, t_seconds, array_wordlists, Array_Temp = [], Array_Sta
                 URL = f'{url}/{Word}'
                 async with s.get(url, ssl=False) as r:
                     if (str(r.status) in Array_Status_Code):
-                        if (URL not in Array_Temp): Array_Temp.append(URL)
+                        if (URL not in Array_Temp):
+                            Array_Temp.append(URL)
+                            Dict_Result[str(r.status)].append(URL)
                     await asyncio.sleep(t_seconds)
 
     asyncio.run(Check_Fuzz())
