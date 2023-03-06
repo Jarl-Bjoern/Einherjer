4d8dd24853f0761fee3d9df0855efd9127745ae2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 18:38:59 +0100

    Update Scan_FTP.py

--- Resources/Workfiles/Scan_FTP.py
+++ Resources/Workfiles/Scan_FTP.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
+
+def Check_FTP(url):
+    ftp = FTP(url, port=22, timeout=15)
+    ftp.login()
+    
+    return Dict_Temp
