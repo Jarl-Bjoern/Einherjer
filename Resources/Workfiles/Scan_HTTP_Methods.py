37b4cdef43cbe0b71fc778d1e2e1fbc7cc933c30
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 18:16:39 +0100

    Update Scan_HTTP_Methods.py

--- Resources/Workfiles/Scan_HTTP_Methods.py
+++ Resources/Workfiles/Scan_HTTP_Methods.py
@@ -24,7 +24,11 @@ def Check_HTTP_Methods(url, Host_Name, Dict_Temp = {'DNS': "", 'CONNECT': "", 'D
                 except ServerDisconnectedError:
                     Dict_Temp[Method] = "FEHLT"
 
-    asyncio.run(Check_Methods())
+    #asyncio.run(Check_Methods())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(Check_Methods())
+    loop.run_until_complete(asyncio.sleep(0.250))
+    loop.close()
 
     if (Host_Name != ""): Logs.Log_File(Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.BLUE+'HTTP-Methods-Check\n'+Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {html_decode(url)} - {Host_Name}'+Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'+Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n')
     else: Logs.Log_File(Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.BLUE+'HTTP-Methods-Check\n'+Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {html_decode(url)}'+Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'+Colors.ORANGE+Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n')
