a496f474cd8db435c9f72b711619db30b61e2597
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 17:55:24 +0100

    Update Scan_Fuzzing.py

--- Resources/Workfiles/Scan_Fuzzing.py
+++ Resources/Workfiles/Scan_Fuzzing.py
@@ -3,11 +3,16 @@
 # Rainer Christian Bjoern Herold
 
 def Check_Site_Paths(url, t_seconds, array_wordlists, Array_Temp = [], Array_Status_Code = ["200", "204", "301", "302", "307", "308", "401", "403", "405", "500"]):
-    for Word in array_wordlists:
-        URL = f'{url}/{Word}'
-        r = get(URL, timeout=t_seconds, verify=False, allow_redirects=True)
-        if (str(r.status_code) in Array_Status_Code):
-            if (URL not in Array_Temp): Array_Temp.append(URL)
-        sleep(t_seconds)
+    async def Check_Fuzz():
+        Limit = TCPConnector(limit_per_host=100)
+        async with ClientSession(connector=Limit, trust_env=True) as s:
+            for Word in array_wordlists:
+                URL = f'{url}/{Word}'
+                async with s.get(url, ssl=False) as r:
+                    if (str(r.status) in Array_Status_Code):
+                        if (URL not in Array_Temp): Array_Temp.append(URL)
+                    sleep(t_seconds)
+
+    asyncio.run(Check_Fuzz())
 
     return Array_Temp
