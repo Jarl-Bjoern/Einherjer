20375c67490628dc27aa59be78eaba37e876c761
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 19:09:44 +0100

    Update Scan_SMTP.py

--- Resources/Workfiles/Scan_SMTP.py
+++ Resources/Workfiles/Scan_SMTP.py
@@ -4,9 +4,18 @@
 
 class Check_SMTP:
     def Check_TLS():
-        pass
+        Mail = SMTP(Target, 25)
+        Mail.ehlo()
+        Mail.starttls()
+        Mail.quit()
+
+    def Test():
+        Mail = SMTP(Target, 25)
+        Mail.docmd()
+        Mail.quit()
 
     def Check_Open_Relay(Target, sender, receivers, message):
         Mail = SMTP(Target, 25)
+        Mail.ehlo()
         Mail.sendmail(sender, receivers, message)         
         Mail.quit()
