69fa49941c19a26be7dce71f346dc40081fe66b8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 18:39:29 +0100

    Update Scan_FTP.py

--- Resources/Workfiles/Scan_FTP.py
+++ Resources/Workfiles/Scan_FTP.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
 
-def Check_FTP(url):
+def Check_FTP(url, Dict_Temp = {'DNS': "", 'Anonymous_Login': ""]):
     ftp = FTP(url, port=22, timeout=15)
     ftp.login()
     
