dab8deb89b89037645cf8db0749569fced2ea55c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-06 20:42:10 +0100

    Update Methods.py

--- Resources/Filter/Methods.py
+++ Resources/Filter/Methods.py
@@ -22,9 +22,7 @@ class Filter:
                 with open(join(Output_Location, 'affected_systems.txt'), 'w') as af:
                     for _ in Target_Array:
                         for Target_DNS in Dict_DNS:
-                            print (_)
-                            print (Target_DNS)
-                            if (Target_DNS in _):
+                            if (Target_DNS in _ or Target_DNS == _):
                                 f.write(f'{Dict_DNS[Target_DNS]}\n')
                                 af.write(f'{_} ({Dict_DNS[Target_DNS]})\n')
                                 break
