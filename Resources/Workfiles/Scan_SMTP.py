c7bf1472b750d0b9a8b8aaa1f649b18f3aa3f351
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-07 20:43:51 +0100

    Update Scan_SMTP.py

--- Resources/Workfiles/Scan_SMTP.py
+++ Resources/Workfiles/Scan_SMTP.py
@@ -3,14 +3,21 @@
 # Rainer Christian Bjoern Herold
 
 class Check_SMTP:
-    def Check_TLS():
+    def Check_TLS(Target):
         Mail = SMTP(Target, 25)
         Mail.ehlo()
-        Mail.starttls()
-        Mail.ehlo()
-        Mail.quit()
+        try:
+                Mail.starttls()
+                Mail.ehlo()
+        except SSLZeroReturnError:
+            print ("Unencrypted")
+        finally:
+            try:
+                Mail.quit()
+            except SMTPServerDisconnected:
+                pass
 
-    def Test():
+    def Test(Target):
         Mail = SMTP(Target, 25)
         Mail.docmd()
         Mail.quit()
@@ -18,5 +25,5 @@ class Check_SMTP:
     def Check_Open_Relay(Target, sender, receivers, message):
         Mail = SMTP(Target, 25)
         Mail.ehlo()
-        Mail.sendmail(sender, receivers, message)         
+        Mail.sendmail(sender, receivers, message)
         Mail.quit()
