0a8066ca85c7d470e3b94a4608d0051d8b97de84
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-09 22:04:30 +0100

    Update main.py

--- Resources/main.py
+++ Resources/main.py
@@ -18,7 +18,7 @@ def main(Date, Program_Mode, args, Array_Output = []):
         if (len(listdir(Check_Dir)) == 0):
             Chromium_Version = getoutput('apt-cache policy chromium').splitlines()[1][1:].split(':')[1][1:]
             if (osname != 'nt'):
-                if (Chromedriver_Version in Chromium_Version):
+                if (Chromedriver_Version[:-4] in Chromium_Version):
                     Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\tUnfortunately, it was not possible to establish a connection via the webdriver, possibly the target system has a WAF in use, as the versions of the Chromedriver and Chromium match."+Colors.RESET, 0.01)
                 else:
                     Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\t    It was not possible to use the current chromium and webdriver version.\n\n\t\t\t\tChromium:             "+Colors.RED+f"{Chromium_Version}\n\t\t\t\t"+Colors.ORANGE+"Chromedriver Version: "+Colors.RED+f"{Chromedriver_Version}"+Colors.RESET, 0.01)
