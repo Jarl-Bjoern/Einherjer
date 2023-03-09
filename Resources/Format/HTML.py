6c9a71e642a4f035db7632652cd56f2f0bf0f0d1
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-09 21:43:35 +0100

    Update HTML.py

--- Resources/Format/HTML.py
+++ Resources/Format/HTML.py
@@ -3,7 +3,7 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from Header_Files.Variables import *
+from ..Header_Files.Variables import *
 
 def HTML_Table(Dict_Result, location, Body_HTML = ""):
     Head_HTML = f"""<!DOCTYPE html>
