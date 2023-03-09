679c4507090741cb976c1d585b7b490697def261
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-09 21:26:35 +0100

    Update Libraries.py

--- Resources/Header_Files/Libraries.py
+++ Resources/Header_Files/Libraries.py
@@ -53,6 +53,7 @@ try:
 
     # Argument_Parser
     try:
+        print (argv)
         if (argv[1] == "--filter-mode"):
             argv.remove('--filter-mode')
             from .ArgParser_Filter import Argument_Parser
@@ -64,6 +65,8 @@ try:
             args, Program_Mode = Argument_Parser(), "Brute_Force_Mode"
         elif (argv[1] == "--scanning-mode"):
             argv.remove('--scanning-mode')
+            print (argv)
+            exit()
             from .ArgParser_Scan import Argument_Parser
             args, Program_Mode = Argument_Parser(), "Scanning_Mode"
 
