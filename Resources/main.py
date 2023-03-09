5b2aa0c5cd12974a13e36ddca1ff1ff55fd43a04
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-09 19:29:44 +0100

    Update main.py

--- Resources/main.py
+++ Resources/main.py
@@ -303,7 +303,7 @@ def main(Date, Program_Mode, args, Array_Output = []):
                     Chromium_Version = getoutput('apt-cache policy chromium').splitlines()[1][1:].split(':')[1][1:]
                     if (osname != 'nt'):
                         if (Chromedriver_Version in Chromium_Version):
-                            Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\t    It was not possible to use the current chromium and webdriver version.\n\nChromium: "+Colors.RED+f"{Chromium_Version}\n"+Colors.ORANGE+"Chromedriver Version: "+Colors.RED+f"{Chromedriver_Version}"+Colors.RESET, 0.01)
+                            Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\tUnfortunately, it was not possible to establish a connection via the webdriver, possibly the target system has a WAF in use, as the versions of the Chromedriver and Chromium match."+Colors.RESET, 0.01)
                         else:
                             Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\t    It was not possible to use the current chromium and webdriver version.\n\nChromium: "+Colors.RED+f"{Chromium_Version}\n"+Colors.ORANGE+"Chromedriver Version: "+Colors.RED+f"{Chromedriver_Version}"+Colors.RESET, 0.01)
                     else:
@@ -320,7 +320,7 @@ def main(Date, Program_Mode, args, Array_Output = []):
                     Chromium_Version = getoutput('apt-cache policy chromium').splitlines()[1][1:].split(':')[1][1:]
                     if (osname != 'nt'):
                         if (Chromedriver_Version in Chromium_Version):
-                            Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\t    It was not possible to use the current chromium and webdriver version.\n\nChromium: "+Colors.RED+f"{Chromium_Version}\n"+Colors.ORANGE+"Chromedriver Version: "+Colors.RED+f"{Chromedriver_Version}"+Colors.RESET, 0.01)
+                            Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\tUnfortunately, it was not possible to establish a connection via the webdriver, possibly the target system has a WAF in use, as the versions of the Chromedriver and Chromium match."+Colors.RESET, 0.01)
                         else:
                             Standard.Stdout_Output(Colors.ORANGE+"\n\n\t\t    It was not possible to use the current chromium and webdriver version.\n\nChromium: "+Colors.RED+f"{Chromium_Version}\n"+Colors.ORANGE+"Chromedriver Version: "+Colors.RED+f"{Chromedriver_Version}"+Colors.RESET, 0.01)
                     else:
