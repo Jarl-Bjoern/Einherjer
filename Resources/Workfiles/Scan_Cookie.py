115effead6791a43fc093616ad928c4d908f168d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-12 10:23:58 +0100

    Update Scan_Cookie.py

--- Resources/Workfiles/Scan_Cookie.py
+++ Resources/Workfiles/Scan_Cookie.py
@@ -7,13 +7,24 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Temp = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
+def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Dict_Temp = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
+    # Session_Creation
     s = Session()
-    if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
+
+    # Auth_Configuration
+    if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+        r = s.get(url, timeout=(t_seconds, t_seconds), verify=False, allow_redirects=True, proxies=Dict_Proxies, auth=(Dict_Auth['user'], Dict_Auth['password']))
+
+    elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+        r = s.get(url, timeout=(t_seconds, t_seconds), verify=False, allow_redirects=True, auth=(Dict_Auth['user'], Dict_Auth['password']))
+
+    elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
         r = s.get(url, timeout=(t_seconds, t_seconds), verify=False, allow_redirects=True, proxies=Dict_Proxies)
-    else:
+
+    elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
         r = s.get(url, timeout=(t_seconds, t_seconds), verify=False, allow_redirects=True)
 
+    # Get_Host_Name
     if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
     else: Dict_Temp['DNS'] = ""
 
@@ -29,6 +40,8 @@ def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Temp = {'
                         else: Dict_Temp[Flag] = "FEHLT"
                         Switch_SameSite = True
                     else: Dict_Temp[Flag] = Flag
+
+    # Empty_Filter
     if (Dict_Temp['SAMESITE'] == ""): Dict_Temp['SAMESITE'] = "FEHLT"
     if (Dict_Temp['HTTPONLY'] == ""): Dict_Temp['HTTPONLY'] = "FEHLT"
     if (Dict_Temp['SECURE'] == ""): Dict_Temp['SECURE'] = "FEHLT"
@@ -50,6 +63,7 @@ def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Temp = {'
 #
 #                        Dict_Result['Security_Flag']
 
+    # Logging
     if (Host_Name != ""): Logs.Log_File(Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.BLUE+'Cookie-Check\n'+Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {html_decode(url)} - {Host_Name} - '+Colors.CYAN+f'{r}'+Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'+Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'+Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'+Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n')
     else: Logs.Log_File(Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.BLUE+'Cookie-Check\n'+Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {html_decode(url)} - '+Colors.CYAN+f'{r}'+Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'+Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'+Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'+Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n')
 
