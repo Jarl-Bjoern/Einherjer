bb0fb719adffc338f5decc36f369c0f75778062c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-12 22:05:35 +0100

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_context(), Dict_Temp = {'DNS': "", 'Issuer': "", 'Subject': "", 'Signature_Algorithm': "", 'Cert_Creation_Date': "", 'Cert_EOL': "", 'Date_Difference': "", 'Current_Date': ""}):
+def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_context(), Dict_Temp = {}):
     if ('https://' in url): URL = url.split('https://')[1]
     elif ('http://' in url): URL = url.split('http://')[1]
     else: URL = url
@@ -23,7 +23,6 @@ def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_con
     else: Dict_Temp['DNS'] = ""
 
     try:
-        Current_Date = datetime.now()
         with create_connection((URL, int(Port)), timeout=t_seconds) as sock:
             with context.wrap_socket(sock, server_hostname=URL) as ssock:
                 cert_der = ssock.getpeercert(True)
@@ -33,6 +32,7 @@ def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_con
                 #public_key = cert_key.get_pubkey()
                 #rsa_key = public_key.get_rsa()
                 #cipher = rsa_key.public_encrypt('plaintext', RSA.pkcs1_padding)
+                Current_Date = datetime.now()
                 Dict_Temp['Issuer'] = str(cert.issuer)
                 Dict_Temp['Subject'] = str(cert.subject)
                 Dict_Temp['Signature_Algorithm'] = str(cert.signature_algorithm_oid).split('name=')[1][:-2].upper()
@@ -45,12 +45,12 @@ def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_con
                 if (Date_Difference < 0): Dict_Temp['Date_Difference'] = f'{str(Date_Difference)[1:]} days before expires'
                 else: Dict_Temp['Date_Difference'] = f'expired since {Date_Difference} days'
 
+                # Add_Scan_Date
+                Dict_Temp['Current_Date'] = str(Current_Date).split('.')[0]
+
         # Logging
         if (Host_Name != ""): Logs.Log_File(Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.BLUE+'Certificate-Check\n'+Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {html_decode(url)} - {Host_Name} - '+Colors.CYAN+'Certificate Information was succesfully recorded.\n\n')
         else: Logs.Log_File(Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.BLUE+'Certificate-Check\n'+Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'+Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {html_decode(url)} - '+Colors.CYAN+'Certificate Information was successfully recorded.\n\n')
     except (ConnectionRefusedError, gaierror, SSLError): Logs.Write_Log(html_decode(url), Host_Name)
-
-    # Add_Scan_Date
-    Dict_Temp['Current_Date'] = str(Current_Date).split('.')[0]
 
     return Dict_Temp
