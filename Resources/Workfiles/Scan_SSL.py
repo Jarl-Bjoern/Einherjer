4ff7de345e4e847327cf38fbe76b9b483199ccaf
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-14 21:23:49 +0100

    Update Scan_SSL.py

--- Resources/Workfiles/Scan_SSL.py
+++ Resources/Workfiles/Scan_SSL.py
@@ -102,7 +102,7 @@ def SSL_Vulns(array_ssl_targets, ssl_timeout, Array_Result_Filter = ['http_heade
 
             for _ in json_loads(temp_json_output)['server_scan_results']:
                 Host_Name = Get_Host_Name(_['server_location']['ip_address'])
-                Dict_SSL_Vulns['Target'], Dict_Full_SSL['DNS'] = f"{_['server_location']['ip_address']}:{_['server_location']['port']}"
+                Dict_SSL_Vulns['Target'], Dict_Full_SSL['DNS'] = f"{_['server_location']['ip_address']}:{_['server_location']['port']}", Host_Name
                 Scan_Result = _['scan_result']
                 if (Scan_Result != None):
                     for i in Scan_Result:
