db82694e45e35fd49f792683fa94452c1638d260
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-14 18:58:42 +0100

    Update Threads_SSL.py

--- Resources/Header_Files/Threads_SSL.py
+++ Resources/Header_Files/Threads_SSL.py
@@ -14,7 +14,9 @@ def Thread_SSL_Start(array_ssl, t_seconds, queue, dict_switch, ssl_timeout, dict
 
         # SSL
         if (dict_switch['scan_ssl'] != False):
-            Dict_Result['SSL'][url] = SSL_Vulns(array_ssl, ssl_timeout)
+            #Dict_Result['SSL'][url] = 
+            print ("TestScan")
+            SSL_Vulns(array_ssl, ssl_timeout)
 
     except (ConnectionError, gaierror, RequestException):
         Logs.Write_Log(url)
