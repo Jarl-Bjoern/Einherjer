52f50bfbfdd81166755b51d7c0a276ff4cafdf19
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-14 22:06:24 +0100

    Update Scan_Cookie.py

--- Resources/Workfiles/Scan_Cookie.py
+++ Resources/Workfiles/Scan_Cookie.py
@@ -9,67 +9,66 @@ from ..Standard_Operations.Colors import Colors
 
 def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Dict_Temp = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
     # Session_Creation
-    s = Session()
+    with Session() as s:
+        # Auth_Configuration
+        if (Dict_Auth['pkcs12_cert'] != ''):
+            s.mount(url, Pkcs12Adapter(pkcs12_filename=Dict_Auth['pkcs12_cert'], pkcs12_password=Dict_Auth['pkcs12_password']))
+            if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
+                r = s.get(
+                    url,
+                    timeout=(t_seconds, t_seconds),
+                    verify=False,
+                    allow_redirects=True,
+                    proxies=Dict_Proxies,
+                    pkcs12_filename=Dict_Auth['pkcs12_cert'],
+                    pkcs12_password=Dict_Auth['pkcs12_password']
+                )
 
-    # Auth_Configuration
-    if (Dict_Auth['pkcs12_cert'] != ''):
-        s.mount(url, Pkcs12Adapter(pkcs12_filename=Dict_Auth['pkcs12_cert'], pkcs12_password=Dict_Auth['pkcs12_password']))
-        if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
-            r = s.get(
-                url,
-                timeout=(t_seconds, t_seconds),
-                verify=False,
-                allow_redirects=True,
-                proxies=Dict_Proxies,
-                pkcs12_filename=Dict_Auth['pkcs12_cert'],
-                pkcs12_password=Dict_Auth['pkcs12_password']
-            )
+            elif (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == ''):
+                r = s.get(
+                    url,
+                    timeout=(t_seconds, t_seconds),
+                    verify=False,
+                    allow_redirects=True,
+                    pkcs12_filename=Dict_Auth['pkcs12_cert'],
+                    pkcs12_password=Dict_Auth['pkcs12_password']
+                )
+        else:
+            if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+                r = s.get(
+                    url,
+                    timeout=(t_seconds, t_seconds),
+                    verify=False,
+                    allow_redirects=True,
+                    proxies=Dict_Proxies,
+                    auth=(Dict_Auth['user'], Dict_Auth['password'])
+                )
 
-        elif (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == ''):
-            r = s.get(
-                url,
-                timeout=(t_seconds, t_seconds),
-                verify=False,
-                allow_redirects=True,
-                pkcs12_filename=Dict_Auth['pkcs12_cert'],
-                pkcs12_password=Dict_Auth['pkcs12_password']
-            )
-    else:
-        if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
-            r = s.get(
-                url,
-                timeout=(t_seconds, t_seconds),
-                verify=False,
-                allow_redirects=True,
-                proxies=Dict_Proxies,
-                auth=(Dict_Auth['user'], Dict_Auth['password'])
-            )
+            elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+                r = s.get(
+                    url,
+                    timeout=(t_seconds, t_seconds),
+                    verify=False,
+                    allow_redirects=True,
+                    auth=(Dict_Auth['user'], Dict_Auth['password'])
+                )
 
-        elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
-            r = s.get(
-                url,
-                timeout=(t_seconds, t_seconds),
-                verify=False,
-                allow_redirects=True,
-                auth=(Dict_Auth['user'], Dict_Auth['password'])
-            )
+            elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+                r = s.get(
+                    url,
+                    timeout=(t_seconds, t_seconds),
+                    verify=False,
+                    allow_redirects=True,
+                    proxies=Dict_Proxies
+                )
 
-        elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
-            r = s.get(
-                url,
-                timeout=(t_seconds, t_seconds),
-                verify=False,
-                allow_redirects=True,
-                proxies=Dict_Proxies
-            )
-
-        elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
-            r = s.get(
-                url,
-                timeout=(t_seconds, t_seconds),
-                verify=False,
-                allow_redirects=True
-            )
+            elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+                r = s.get(
+                    url,
+                    timeout=(t_seconds, t_seconds),
+                    verify=False,
+                    allow_redirects=True
+                )
 
     # Get_Host_Name
     if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
