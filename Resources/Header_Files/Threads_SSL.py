d77e5a752c599b2b34f93ae0203b94f6ea54334f
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-14 17:54:09 +0100

    Create Threads_SSL.py

---
+++ Resources/Header_Files/Threads_SSL.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Libraries
+from .Variables import *
+from ..Standard_Operations.Logs import *
+from ..Workfiles.Scan_Host_Name import Get_Host_Name
+from ..Workfiles.Scan_SSL import SSL_Vulns
+
+# Functions
+def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout, ssl_timeout, dict_proxies, dict_auth, Host_Name = "", Target = ""):
+    try:
+        Dict_Result = queue.get()
+
+        # Get_Host_Name
+        Host_Name = Get_Host_Name(url)
+
+        # SSL
+        if (dict_switch['scan_ssl'] != False):
+            Dict_Result['SSL'][url] = SSL_Vulns(array_ssl, ssl_timeout, Host_Name)
+
+    except (ConnectionError, gaierror, WebDriverException, RequestException):
+        Logs.Write_Log(url, Host_Name)
+    finally:
+        queue.put(Dict_Result)
