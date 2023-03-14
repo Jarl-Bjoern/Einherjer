90b4a53a848dbcfd5ce54e8e28207dd768698c4e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-14 17:54:34 +0100

    Update Threads_SSL.py

--- Resources/Header_Files/Threads_SSL.py
+++ Resources/Header_Files/Threads_SSL.py
@@ -9,7 +9,7 @@ from ..Workfiles.Scan_Host_Name import Get_Host_Name
 from ..Workfiles.Scan_SSL import SSL_Vulns
 
 # Functions
-def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch_internet_connection, screenshot_wait, webdriver_timeout, ssl_timeout, dict_proxies, dict_auth, Host_Name = "", Target = ""):
+def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, ssl_timeout, dict_proxies, dict_auth, Host_Name = "", Target = ""):
     try:
         Dict_Result = queue.get()
 
