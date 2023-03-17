c30799280c353689ff877f9a2dd26e7c85b4d0cf
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-17 22:30:48 +0100

    Update Scan_Cookie.py

--- Resources/Workfiles/Scan_Cookie.py
+++ Resources/Workfiles/Scan_Cookie.py
@@ -71,27 +71,49 @@ def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Dic
                     allow_redirects=True
                 )
 
-    # Get_Host_Name
-    if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
-    else: Dict_Temp['DNS'] = ""
+        # Get_Host_Name
+        if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
+        else: Dict_Temp['DNS'] = ""
 
-    # Normal_Cookie
-    for Header_Key, Header_Values in r.headers.items():
-        if ("COOKIE" in Header_Key.upper()):
-            Target_Flags = Header_Values.upper()
-            for Flag in Array_Security_Flags:
-                if (Flag not in Target_Flags): Dict_Temp[Flag] = "FEHLT"
-                else:
-                    if ("SAMESITE" in Target_Flags and Switch_SameSite != True):
-                        if ("SAMESITE=LAX" in Target_Flags or "SAMESITE=STRICT" in Target_Flags): Dict_Temp[Flag] = Flag
-                        else: Dict_Temp[Flag] = "FEHLT"
-                        Switch_SameSite = True
-                    else: Dict_Temp[Flag] = Flag
+        # Normal_Cookie
+        for Header_Key, Header_Values in r.headers.items():
+            if ("COOKIE" in Header_Key.upper()):
+                Target_Flags = Header_Values.upper()
+                for Flag in Array_Security_Flags:
+                    if (Flag not in Target_Flags): Dict_Temp[Flag] = "FEHLT"
+                    else:
+                        if ("SAMESITE" in Target_Flags and Switch_SameSite != True):
+                            if ("SAMESITE=LAX" in Target_Flags or "SAMESITE=STRICT" in Target_Flags): Dict_Temp[Flag] = Flag
+                            else: Dict_Temp[Flag] = "FEHLT"
+                            Switch_SameSite = True
+                        else: Dict_Temp[Flag] = Flag
 
-    # Empty_Filter
-    if (Dict_Temp['SAMESITE'] == ""): Dict_Temp['SAMESITE'] = "FEHLT"
-    if (Dict_Temp['HTTPONLY'] == ""): Dict_Temp['HTTPONLY'] = "FEHLT"
-    if (Dict_Temp['SECURE'] == ""): Dict_Temp['SECURE'] = "FEHLT"
+        # Logging
+        if (Host_Name != ""):
+            Logs.Log_File(
+                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.BLUE+'Cookie-Check\n'
+                +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - {Host_Name} - '+Colors.CYAN+f'{r}'
+                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
+                +Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'
+                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
+                +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n'
+            )
+        else:
+            Logs.Log_File(
+                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.BLUE+'Cookie-Check\n'
+                +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.CYAN+f'{r}'
+                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
+                +Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'
+                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
+                +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n'
+            )
+
+        # Terminate_Session
+        r.close()
 
     # Cookie_Jar
 #    for cookie in dict(s.cookies): pass
@@ -110,28 +132,9 @@ def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Dic
 #
 #                        Dict_Result['Security_Flag']
 
-    # Logging
-    if (Host_Name != ""):
-        Logs.Log_File(
-            Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-            +Colors.BLUE+'Cookie-Check\n'
-            +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-            +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - {Host_Name} - '+Colors.CYAN+f'{r}'
-            +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
-            +Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'
-            +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
-            +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n'
-        )
-    else:
-        Logs.Log_File(
-            Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-            +Colors.BLUE+'Cookie-Check\n'
-            +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-            +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.CYAN+f'{r}'
-            +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
-            +Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'
-            +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
-            +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n'
-        )
+    # Empty_Filter
+    if (Dict_Temp['SAMESITE'] == ""): Dict_Temp['SAMESITE'] = "FEHLT"
+    if (Dict_Temp['HTTPONLY'] == ""): Dict_Temp['HTTPONLY'] = "FEHLT"
+    if (Dict_Temp['SECURE'] == ""): Dict_Temp['SECURE'] = "FEHLT"
 
     return Dict_Temp
