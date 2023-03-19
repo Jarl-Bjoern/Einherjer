498d291f356b885d256eeab02812b6bfd21562a0
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 19:01:49 +0100

    Update main.py

--- Resources/main.py
+++ Resources/main.py
@@ -444,7 +444,11 @@ def main(Date, Program_Mode, args, Array_Output = []):
     # Output_End
     if (Array_Output != []):
         if (Program_Mode == "Scanning_Mode"):
-            if (args.scan_site_screenshot != False or args.scan_site_screenshot_recursive != False):
+            if (args.scan_site_screenshot != False):
+                Message_Chromium(join(Location, 'Screenshots'))
+            Standard.Stdout_Output(Colors.CYAN+"\n\nYour Scan was successful and the result will be found at the following location:\n"+Colors.RESET, 0.01)
+        elif (Program_Mode == "Brute_Force_Mode"):
+            if (args.brute_screenshot_recursive != False):
                 Message_Chromium(join(Location, 'Screenshots'))
             Standard.Stdout_Output(Colors.CYAN+"\n\nYour Scan was successful and the result will be found at the following location:\n"+Colors.RESET, 0.01)
         elif (Program_Mode == "Filter_Mode"):
@@ -454,6 +458,10 @@ def main(Date, Program_Mode, args, Array_Output = []):
     else:
         if (Program_Mode == "Scanning_Mode"):
             if (args.scan_site_screenshot != False or args.scan_site_screenshot_recursive != False):
+                Message_Chromium(join(Location, 'Screenshots'))
+            Standard.Stdout_Output(Colors.ORANGE+f'\n\t\t\t\tIt was not possible to collect any kind of data!\n\n\t\t\t     Check your connection or target file and try it again.'+Colors.RESET, 0.01)
+        elif (Program_Mode == "Brute_Force_Mode"):
+             if (args.brute_screenshot_recursive != False):
                 Message_Chromium(join(Location, 'Screenshots'))
             Standard.Stdout_Output(Colors.ORANGE+f'\n\t\t\t\tIt was not possible to collect any kind of data!\n\n\t\t\t     Check your connection or target file and try it again.'+Colors.RESET, 0.01)
         elif (Program_Mode == "Filter_Mode"):
