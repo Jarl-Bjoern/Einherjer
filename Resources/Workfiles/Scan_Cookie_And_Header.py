a2168a39a804841bc2cf38825cfc893414fae96c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 16:35:44 +0100

    Update Scan_Cookie_And_Header.py

--- Resources/Workfiles/Scan_Cookie_And_Header.py
+++ Resources/Workfiles/Scan_Cookie_And_Header.py
@@ -7,74 +7,82 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Site_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Location, Dict_Temp_Header = {}, Dict_Temp_Information_Disclosure = {}):
+def Check_Site_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Location, Dict_Temp_Header = {}, Dict_Temp_Information_Disclosure = {}, Dict_Temp_Cookie = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
     try:
-        # Auth_Configuration
-        if (Dict_Auth['pkcs12_cert'] != ''):
-            if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
-                r = pkcs_get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    proxies=Dict_Proxies,
-                    pkcs12_filename=Dict_Auth['pkcs12_cert'],
-                    pkcs12_password=Dict_Auth['pkcs12_password']
-                )
+        # Session_Creation
+        with Session() as s:
 
-            elif (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == ''):
-                r = pkcs_get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    pkcs12_filename=Dict_Auth['pkcs12_cert'],
-                    pkcs12_password=Dict_Auth['pkcs12_password']
-                )
-        else:
-            if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
-                r = get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    proxies=Dict_Proxies,
-                    auth=(Dict_Auth['user'],Dict_Auth['password'])
-                )
+            # Auth_Configuration
+            if (Dict_Auth['pkcs12_cert'] != ''):
+                s.mount(url, Pkcs12Adapter(pkcs12_filename=Dict_Auth['pkcs12_cert'], pkcs12_password=Dict_Auth['pkcs12_password']))
+                if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        verify=False,
+                        allow_redirects=True,
+                        proxies=Dict_Proxies,
+                        pkcs12_filename=Dict_Auth['pkcs12_cert'],
+                        pkcs12_password=Dict_Auth['pkcs12_password']
+                    )
 
-            elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
-                r = get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    auth=(Dict_Auth['user'], Dict_Auth['password'])
-                )
+                elif (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == ''):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        verify=False,
+                        allow_redirects=True,
+                        pkcs12_filename=Dict_Auth['pkcs12_cert'],
+                        pkcs12_password=Dict_Auth['pkcs12_password']
+                    )
+            else:
+                if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        verify=False,
+                        allow_redirects=True,
+                        proxies=Dict_Proxies,
+                        auth=(Dict_Auth['user'], Dict_Auth['password'])
+                    )
 
-            elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
-                r = get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    proxies=Dict_Proxies
-                )
+                elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        verify=False,
+                        allow_redirects=True,
+                        auth=(Dict_Auth['user'], Dict_Auth['password'])
+                    )
 
-            elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
-                r = get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True
-                )
+                elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        verify=False,
+                        allow_redirects=True,
+                        proxies=Dict_Proxies
+                    )
+
+                elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        verify=False,
+                        allow_redirects=True
+                    )
 
         # Get_Host_Name
         if (Host_Name != ""):
-            Dict_Temp_Header['DNS'], Dict_Temp_Information_Disclosure['DNS'] = Host_Name, Host_Name
+            Dict_Temp_Cookie['DNS']                 = Host_Name
+            Dict_Temp_Header['DNS']                 = Host_Name
+            Dict_Temp_Information_Disclosure['DNS'] = Host_Name
         else:
-            Dict_Temp_Header['DNS'], Dict_Temp_Information_Disclosure['DNS'] = "",""
+            Dict_Temp_Cookie['DNS']                 = ""
+            Dict_Temp_Header['DNS']                 = ""
+            Dict_Temp_Information_Disclosure['DNS'] = ""
 
-        # Scanning_Process
+        # Scanning_Process_Header
         for Header in r.headers.items():
             # Check_Header
             if (Header[0].upper() in Dict_Header):
@@ -97,6 +105,19 @@ def Check_Site_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Locati
                                 Dict_Temp_Header[Temp_Head] = Header[1].upper()
                     else:
                         Dict_Temp_Header[Temp_Head] = "FEHLT"
+
+        # Scanning_Cookie
+        for Header_Key, Header_Values in r.headers.items():
+            if ("COOKIE" in Header_Key.upper()):
+                Target_Flags = Header_Values.upper()
+                for Flag in Array_Security_Flags:
+                    if (Flag not in Target_Flags): Dict_Temp[Flag] = "FEHLT"
+                    else:
+                        if ("SAMESITE" in Target_Flags and Switch_SameSite != True):
+                            if ("SAMESITE=LAX" in Target_Flags or "SAMESITE=STRICT" in Target_Flags): Dict_Temp[Flag] = Flag
+                            else: Dict_Temp[Flag] = "FEHLT"
+                            Switch_SameSite = True
+                        else: Dict_Temp[Flag] = Flag
 
             # Check_HTTP_Information_Header
             elif (Header[0].upper() in Array_Information_Disclosure_Header):
@@ -133,6 +154,17 @@ def Check_Site_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Locati
                     +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp_Information_Disclosure}\n\n',
                     join(Location, 'Logs')
                 )
+            Logs.Log_File(
+                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.BLUE+'Cookie-Check\n'
+                +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - {Host_Name} - '+Colors.CYAN+f'{r}'
+                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
+                +Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'
+                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
+                +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n',
+                join(Location, 'Logs')
+            )
         else:
             Logs.Log_File(
                 Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
@@ -157,120 +189,6 @@ def Check_Site_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Locati
                     +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp_Information_Disclosure}\n\n',
                     join(Location, 'Logs')
                 )
-
-        # Terminate_Session
-        r.close()
-
-    except ReadTimeout:
-        Logs.Write_Log(url, Host_Name, join(Location, 'Logs'))
-
-    return Dict_Temp_Header, Dict_Temp_Information_Disclosure
-
-  
-  #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Rainer Christian Bjoern Herold
-
-# Libraries
-from ..Header_Files.Variables import *
-from ..Standard_Operations.Logs import Logs
-from ..Standard_Operations.Colors import Colors
-
-def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Location, Dict_Temp = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
-    # Session_Creation
-    with Session() as s:
-
-        # Auth_Configuration
-        if (Dict_Auth['pkcs12_cert'] != ''):
-            s.mount(url, Pkcs12Adapter(pkcs12_filename=Dict_Auth['pkcs12_cert'], pkcs12_password=Dict_Auth['pkcs12_password']))
-            if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
-                r = s.get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    proxies=Dict_Proxies,
-                    pkcs12_filename=Dict_Auth['pkcs12_cert'],
-                    pkcs12_password=Dict_Auth['pkcs12_password']
-                )
-
-            elif (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == ''):
-                r = s.get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    pkcs12_filename=Dict_Auth['pkcs12_cert'],
-                    pkcs12_password=Dict_Auth['pkcs12_password']
-                )
-        else:
-            if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
-                r = s.get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    proxies=Dict_Proxies,
-                    auth=(Dict_Auth['user'], Dict_Auth['password'])
-                )
-
-            elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
-                r = s.get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    auth=(Dict_Auth['user'], Dict_Auth['password'])
-                )
-
-            elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
-                r = s.get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True,
-                    proxies=Dict_Proxies
-                )
-
-            elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
-                r = s.get(
-                    url,
-                    timeout=(t_seconds, t_seconds),
-                    verify=False,
-                    allow_redirects=True
-                )
-
-        # Get_Host_Name
-        if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
-        else: Dict_Temp['DNS'] = ""
-
-        # Normal_Cookie
-        for Header_Key, Header_Values in r.headers.items():
-            if ("COOKIE" in Header_Key.upper()):
-                Target_Flags = Header_Values.upper()
-                for Flag in Array_Security_Flags:
-                    if (Flag not in Target_Flags): Dict_Temp[Flag] = "FEHLT"
-                    else:
-                        if ("SAMESITE" in Target_Flags and Switch_SameSite != True):
-                            if ("SAMESITE=LAX" in Target_Flags or "SAMESITE=STRICT" in Target_Flags): Dict_Temp[Flag] = Flag
-                            else: Dict_Temp[Flag] = "FEHLT"
-                            Switch_SameSite = True
-                        else: Dict_Temp[Flag] = Flag
-
-        # Logging
-        if (Host_Name != ""):
-            Logs.Log_File(
-                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-                +Colors.BLUE+'Cookie-Check\n'
-                +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-                +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - {Host_Name} - '+Colors.CYAN+f'{r}'
-                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
-                +Colors.ORANGE+'\nOriginal Output'+Colors.RED+' -> '+Colors.RESET+f'{r.headers.items()}'
-                +Colors.BLUE+'\n-----------------------------------------------------------------------------------------------------------'
-                +Colors.ORANGE+'\nEinherjer Filter'+Colors.RED+' -> '+Colors.RESET+f'{Dict_Temp}\n\n',
-                join(Location, 'Logs')
-            )
-        else:
             Logs.Log_File(
                 Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
                 +Colors.BLUE+'Cookie-Check\n'
@@ -286,26 +204,12 @@ def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Loc
         # Terminate_Session
         r.close()
 
-    # Cookie_Jar
-#    for cookie in dict(s.cookies): pass
-#        for i,j in cookie.__dict__.items():
-#            if ('_rest' in i):
-#                print (f'{i} : {j}')
-
-#                        try:
-#                            Cookie = r.cookies.get_dict()
-#                            for head in Cookie:
-#                                if ('JSID' not in head):
-#
-#                                elif ('Test' not in head):
-#                        except: pass
-#
-#
-#                        Dict_Result['Security_Flag']
+    except ReadTimeout:
+        Logs.Write_Log(url, Host_Name, join(Location, 'Logs'))
 
     # Empty_Filter
-    if (Dict_Temp['SAMESITE'] == ""): Dict_Temp['SAMESITE'] = "FEHLT"
-    if (Dict_Temp['HTTPONLY'] == ""): Dict_Temp['HTTPONLY'] = "FEHLT"
-    if (Dict_Temp['SECURE'] == ""): Dict_Temp['SECURE'] = "FEHLT"
+    if (Dict_Temp_Cookie['SAMESITE'] == ""): Dict_Temp_Cookie['SAMESITE'] = "FEHLT"
+    if (Dict_Temp_Cookie['HTTPONLY'] == ""): Dict_Temp_Cookie['HTTPONLY'] = "FEHLT"
+    if (Dict_Temp_Cookie['SECURE'] == ""): Dict_Temp_Cookie['SECURE'] = "FEHLT"
 
-    return Dict_Temp
+    return Dict_Temp_Cookie, Dict_Temp_Header, Dict_Temp_Information_Disclosure
