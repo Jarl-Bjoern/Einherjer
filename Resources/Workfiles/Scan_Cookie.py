c1c624c8e596c28f40b464e564aa8704f0b7717b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 12:24:55 +0100

    Update Scan_Cookie.py

--- Resources/Workfiles/Scan_Cookie.py
+++ Resources/Workfiles/Scan_Cookie.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Dict_Temp = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
+def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, file_format, Location, Dict_Temp = {'DNS': "", 'SAMESITE': "", 'HTTPONLY': "", 'SECURE': ""}, Switch_SameSite = False):
     # Session_Creation
     with Session() as s:
 
@@ -136,5 +136,39 @@ def Check_Security_Flags(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Dic
     if (Dict_Temp['SAMESITE'] == ""): Dict_Temp['SAMESITE'] = "FEHLT"
     if (Dict_Temp['HTTPONLY'] == ""): Dict_Temp['HTTPONLY'] = "FEHLT"
     if (Dict_Temp['SECURE'] == ""): Dict_Temp['SECURE'] = "FEHLT"
+
+    # Format_Filtering
+    if ("csv" in file_format):
+        from ..Format.CSV import CSV_Table
+        Array_Output = CSV_Table(Dict_Temp, Location)
+    elif ("docx" in file_format):
+        from ..Format.Word import Word_Table
+        Array_Output = Word_Table(Dict_Temp, Location)
+    elif ("html" in file_format):
+        from ..Format.HTML import HTML_Table
+        Array_Output = HTML_Table(Dict_Temp, Location)
+    elif ("json" in file_format):
+        from ..Format.JSON import JSON_Table
+        Array_Output = JSON_Table(Dict_Temp, Location)
+    elif ("md" in file_format):
+        from ..Format.Markdown import Markdown_Table
+        Array_Output = Markdown_Table(Dict_Temp, Location)
+    elif ("pdf" in file_format):
+        from ..Format.PDF import Create_PDF
+        Array_Output = Word_Table(Dict_Temp, Location)
+        if (osname == 'nt'): Create_PDF(Location)
+        else: print("At this point it's not be possible to convert a docx file into a pdf under linux.\nPlease try it under windows.\n")
+    elif ("tex" in file_format):
+        from ..Format.LaTeX import Latex_Table
+        Array_Output = Latex_Table(Dict_Temp, Location)
+    elif ("xlsx" in file_format):
+        from ..Format.Excel import Excel_Table
+        Array_Output = Excel_Table(Dict_Temp, Location)
+    elif ("xml" in file_format):
+        from ..Format.XML import XML_Table
+        #Array_Output = XML_Table(Dict_Temp, Location)
+    elif ("yaml" in file_format):
+        from ..Format.YAML import YAML_Table
+        #Array_Output = YAML_Table(Dict_Temp, Location)
 
     return Dict_Temp
