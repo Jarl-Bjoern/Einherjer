9e51cbb798c90e09aa076b7e10c93cbd82e8a93d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 12:48:33 +0100

    Update Scan_Header.py

--- Resources/Workfiles/Scan_Header.py
+++ Resources/Workfiles/Scan_Header.py
@@ -157,40 +157,6 @@ def Check_Site_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, file_f
         # Terminate_Session
         r.close()
 
-        # Format_Filtering
-        #if ("csv" in file_format):
-        #    from ..Format.CSV import CSV_Table
-        #    Array_Output = CSV_Table(Dict_Temp_Header, Location)
-        #elif ("docx" in file_format):
-        #    from ..Format.Word import Word_Table
-        #    Array_Output = Word_Table(Dict_Temp_Header, Location)
-        #elif ("html" in file_format):
-        #    from ..Format.HTML import HTML_Table
-        #    Array_Output = HTML_Table(Dict_Temp_Header, Location)
-        #elif ("json" in file_format):
-        #    from ..Format.JSON import JSON_Table
-        #    Array_Output = JSON_Table(Dict_Temp_Header, Location)
-        #elif ("md" in file_format):
-        #    from ..Format.Markdown import Markdown_Table
-        #    Array_Output = Markdown_Table(Dict_Temp_Header, Location)
-        #elif ("pdf" in file_format):
-        #    from ..Format.PDF import Create_PDF
-        #    Array_Output = Word_Table(Dict_Temp_Header, Location)
-        #    if (osname == 'nt'): Create_PDF(Location)
-        #    else: print("At this point it's not be possible to convert a docx file into a pdf under linux.\nPlease try it under windows.\n")
-        #elif ("tex" in file_format):
-        #    from ..Format.LaTeX import Latex_Table
-        #    Array_Output = Latex_Table(Dict_Temp_Header, Location)
-        #elif ("xlsx" in file_format):
-        #    from ..Format.Excel import Excel_Table
-        #    Array_Output = Excel_Table(Dict_Temp_Header, Location)
-        #elif ("xml" in file_format):
-        #    from ..Format.XML import XML_Table
-        #    #Array_Output = XML_Table(Dict_Temp_Header, Location)
-        #elif ("yaml" in file_format):
-        #    from ..Format.YAML import YAML_Table
-        #    #Array_Output = YAML_Table(Dict_Temp_Header, Location)
-
     except ReadTimeout:
         Logs.Write_Log(url, Host_Name)
 
