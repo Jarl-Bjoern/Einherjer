160f8927a4c84936a94960a0ccc14bcb27f7ca19
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 15:49:38 +0100

    Update Threads_SSL.py

--- Resources/Header_Files/Threads_SSL.py
+++ Resources/Header_Files/Threads_SSL.py
@@ -17,7 +17,21 @@ def Thread_SSL_Start(array_ssl, t_seconds, queue, dict_switch, ssl_timeout, dict
 
         # SSL
         if (dict_switch['scan_ssl'] != False):
+            # Trace_Start
+            Logs.Trace_File(
+                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                +Colors.ORANGE+f'{url}'+Colors.RED+' -> '+Colors.RESET+'Host_Name - '+Colors.BLUE+'Trying to connect'+Colors.RESET,
+                join(Location, 'Logs')
+            )
+
+            # Scan_SSL
             Dict_Result['SSL'].update(SSL_Vulns(array_ssl, ssl_timeout))
+
+            # Trace_End
+            Logs.Trace_File(
+                Colors.ORANGE+f'{url}'+Colors.RED+' <- '+Colors.RESET+'Host_Name - '+Colors.GREEN+'OK'+Colors.RESET,
+                join(Location, 'Logs')
+            )
 
     except (ConnectionError, gaierror, RequestException):
         pass
