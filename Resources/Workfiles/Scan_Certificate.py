a1603bb83255e1374e96f1e9c076e1f0507ebcab
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 17:47:02 +0100

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -42,7 +42,7 @@ def Check_Certificate(url, t_seconds, Host_Name, Location, context = create_unve
                 # Get_Cert_Information
                 Current_Date = datetime.now()
 
-                print (resplit('<| ', str(cert.public_key()))[1].split('.'))
+                print (resplit('<| ', str(cert.public_key()))[1].split('.')[::-1][0])
 
                 Dict_Temp['Issuer']              = str(cert.issuer)[6:-2]
                 Dict_Temp['Subject']             = str(cert.subject)[6:-2]
