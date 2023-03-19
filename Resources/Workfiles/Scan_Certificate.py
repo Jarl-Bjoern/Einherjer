3b20908d47af47c185eed0ae00c12c1cbd9b1241
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 12:23:44 +0100

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_context(), Dict_Temp = {}):
+def Check_Certificate(url, t_seconds, Host_Name, file_format, Location, context = create_unverified_context(), Dict_Temp = {}):
     if ('https://' in url): URL = url.split('https://')[1]
     elif ('ssl://' in url): URL = url.split('ssl://')[1]
 
@@ -79,6 +79,41 @@ def Check_Certificate(url, t_seconds, Host_Name, context = create_unverified_con
                 +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '
                 +Colors.CYAN+'Certificate Information was successfully recorded.\n\n'
             )
+
+            # Format_Filtering
+            if ("csv" in file_format):
+                from ..Format.CSV import CSV_Table
+                Array_Output = CSV_Table(Dict_Temp, Location)
+            elif ("docx" in file_format):
+                from ..Format.Word import Word_Table
+                Array_Output = Word_Table(Dict_Temp, Location)
+            elif ("html" in file_format):
+                from ..Format.HTML import HTML_Table
+                Array_Output = HTML_Table(Dict_Temp, Location)
+            elif ("json" in file_format):
+                from ..Format.JSON import JSON_Table
+                Array_Output = JSON_Table(Dict_Temp, Location)
+            elif ("md" in file_format):
+                from ..Format.Markdown import Markdown_Table
+                Array_Output = Markdown_Table(Dict_Temp, Location)
+            elif ("pdf" in file_format):
+                from ..Format.PDF import Create_PDF
+                Array_Output = Word_Table(Dict_Temp, Location)
+                if (osname == 'nt'): Create_PDF(Location)
+                else: print("At this point it's not be possible to convert a docx file into a pdf under linux.\nPlease try it under windows.\n")
+            elif ("tex" in file_format):
+                from ..Format.LaTeX import Latex_Table
+                Array_Output = Latex_Table(Dict_Temp, Location)
+            elif ("xlsx" in file_format):
+                from ..Format.Excel import Excel_Table
+                Array_Output = Excel_Table(Dict_Temp, Location)
+            elif ("xml" in file_format):
+                from ..Format.XML import XML_Table
+                #Array_Output = XML_Table(Dict_Temp, Location)
+            elif ("yaml" in file_format):
+                from ..Format.YAML import YAML_Table
+                #Array_Output = YAML_Table(Dict_Temp, Location)
+
     except (ConnectionRefusedError, gaierror, SSLError):
         Logs.Write_Log(url, Host_Name)
 
