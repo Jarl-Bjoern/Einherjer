3e2d5c8f1ec8f015d15175b89b027485111eafa0
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-19 14:10:56 +0100

    Update Logs.py

--- Resources/Standard_Operations/Logs.py
+++ Resources/Standard_Operations/Logs.py
@@ -10,19 +10,19 @@ class Logs:
     def Error_Message(x):
         print(x), sleep(2), exit()
 
-    def Trace_File(Text, Log_Path = dirname(realpath(__file__)).replace('Resources/Standard_Operations', 'Logs')):
+    def Trace_File(Text, Log_Path):
         if (not exists(Log_Path)): makedirs(Log_Path)
         with open(join(Log_Path, f'{Date}_tracefile.txt'), 'a') as f:
             f.write(f'{Text}\n')
 
-    def Write_Log(url, host, Log_Path = dirname(realpath(__file__)).replace('Resources/Standard_Operations', 'Logs')):
+    def Write_Log(url, host, Log_Path):
         if (not exists(Log_Path)): makedirs(Log_Path)
         with open(join(Log_Path, f'{Date}_failed-url.txt'), 'a') as f:
             f.write(f'{url}\n')
         if (host != ""): Logs.Log_File(Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.RED+'FAILED\n'+Colors.RESET)
         else: Logs.Log_File(Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.RED+'FAILED\n'+Colors.RESET)
 
-    def Log_File(Text, Log_Path = dirname(realpath(__file__)).replace('Resources/Standard_Operations', 'Logs')):
+    def Log_File(Text, Log_Path):
         if (not exists(Log_Path)): makedirs(Log_Path)
         with open(join(Log_Path, f"{Date}.log"), "a") as f:
             f.write(Text)
