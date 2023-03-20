72e5c0a29804ecbb0671d9549e6df6de45f8f126
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-20 22:48:20 +0100

    Update main.py

--- Resources/main.py
+++ Resources/main.py
@@ -430,11 +430,15 @@ def main(Date, Program_Mode, args, Array_Output = [], Password_Input = ""):
                                     Array_Output.append(join(Location, 'Einherjer_Output.zip'))
 
                                 #if (args.zip_file_password != False):
+                                with AESZipFile('new_test.zip', 'a', compression=ZIP_LZMA, encryption=WZ_AES) as zF:
+                                    zF.setpassword(bytes(Password_Input))
+                                    zF.write(join(root, file))
+
                                 #    with open(join(Location, 'Einherjer_Output.zip'), mode='a', allowZip64=True) as zF:
                                 #        zF.write(join(root, file))
                                 #else:
-                                with open(join(Location, 'Einherjer_Output.zip'), 'a', allowZip64=True) as zF:
-                                    zF.write(join(root, file))
+                                #    with open(join(Location, 'Einherjer_Output.zip'), 'a', allowZip64=True) as zF:
+                                #        zF.write(join(root, file))
                                 remove(join(root, file))
                             else:
                                 if (join(root, file) not in Array_Output):
