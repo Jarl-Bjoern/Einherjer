1f9233e702e66ac979e83f5e2863eb6260c3cdaf
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-21 21:28:20 +0100

    Update Scan_FTP.py

--- Resources/Workfiles/Scan_FTP.py
+++ Resources/Workfiles/Scan_FTP.py
@@ -9,14 +9,14 @@ from ..Standard_Operations.Colors import Colors
 
 class Check_FTP:
     def FTP_Anonymous_Check(url, Dict_Temp = {'DNS': "", 'Anonymous_Login': ""}):
-        if ('ftp://' in url): URL = url.split('ftp://')[1]
-        else: URL = url
+        if ('ftp://' in url):    URL = url.split('ftp://')[1]
+        else:                    URL = url
 
         if (url.count(':') > 1): Port = url.split(':')[2]
-        else: Port = 21
+        else:                    Port = 21
 
-        if (Host_Name != ""): Dict_Temp['DNS'] = Host_Name
-        else: Dict_Temp['DNS'] = ""
+        if (Host_Name != ""):    Dict_Temp['DNS'] = Host_Name
+        else:                    Dict_Temp['DNS'] = ""
 
         ftp = FTP()
         try:
