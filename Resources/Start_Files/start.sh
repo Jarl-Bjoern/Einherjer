42d25d2953abe99700018123a72fed0e87b340c6
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-23 21:05:31 +0100

    Create start.sh

---
+++ Resources/Start_Files/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Rainer Christian Bjoern Herold
+
+# Variables
+TEMP_PATH=$(readlink -f -- "$0")
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-13}
+BASE_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-23}
+
+# Color
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+ORANGE='\033[1;33m'
+NOCOLOR='\033[0m'
+
+# Main
+if [[ -d "$BASE_PATH/venv" ]]; then
+        source "$BASE_PATH/"venv/bin/activate
+        sudo python3 "$SCRIPT_PATH/main.py" "$@"
+        deactivate
+else
+        echo -e "\n${RED}Please use a virtual python environment!${NOCOLOR}\n\nInstruction:\n${CYAN}-------------------------------------\n${ORANGE}virtualenv $BASE_PATH/venv\nsource $BASE_PATH/venv/bin/activate\npip3 install -r Setup/requirements.txt\ndeactivate${NOCOLOR}"
+fi
