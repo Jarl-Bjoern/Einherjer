25ea70f4fc0fbb2086bb2e735116661e25cf5347
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-27 16:18:17 +0200

    Update Markdown.py

--- Resources/Format/Markdown.py
+++ Resources/Format/Markdown.py
@@ -156,7 +156,33 @@ def Markdown_Table(Dict_Result, location, Write_Mode = "", Write_Second_Mode = "
                                     else: Temp_Word += " - |"
                                     md_file.write(f'{Temp_Word}\n')
                     elif (Result_Left == "SSL_Vulns"):
-                        pass
+                        for _ in Result_Right:
+                            Temp_Word = ""
+                            Temp_Word += f"| {Target} |"
+                            if (_ == "POODLE" and Result_Right[_] != "False"):
+                                Temp_Word += ' The system is vulnerable for POODLE (CVE-2014-3566) |'
+                            elif (_ == "CRIME" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for CRIME (CVE-2012-4929) |'
+                            elif (_ == "HEARTBLEED" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for HEARTBLEED (CVE-2014-0160) |'
+                            elif (_ == "CCS_INJECTION" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for CCS_INJECTION (CVE-2014-0224) |'
+                            elif (_ == "ROBOT" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for ROBOT () |'
+                            elif (_ == "CLIENT_RENEGOTIATION_DOS" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for CLIENT_RENEGOTIATION_DOS () |'
+                            elif (_ == "FALLBACK_SCSV" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for FALLBACK_SCSV () |'
+                            elif (_ == "BREACH" and Result_Right[_] != "False"):
+                                Temp_Word += ' The system is vulnerable for BREACH (CVE-2013-3587) |'
+                            elif (_ == "LOGJAM" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for LOGJAM (CVE-2015-4000) |'
+                            elif (_ == "BEAST" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for BEAST () |'
+                            elif (_ == "LUCKY13" and Result_Right[_] != False):
+                                Temp_Word += ' The system is vulnerable for LUCKY13 () |'
+
+                            md_file.write(f'{Temp_Word}\n')
                     elif (Result_Left == "Curves"):
                         pass
 
