6ed806ee63c58052304bacd9befeb28dcc568909
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-27 17:10:21 +0200

    Update Threads_Brute.py

--- Resources/Header_Files/Threads_Brute.py
+++ Resources/Header_Files/Threads_Brute.py
@@ -46,39 +46,8 @@ def Thread_SSL_Start(array_ssl, t_seconds, queue, dict_switch, ssl_timeout, dict
             pass
 
 
-        # Format_Filtering
-        if ("csv" in file_format):
-            from ..Format.CSV import CSV_Table
-            CSV_Table(Dict_Temp, Location)
-        elif ("docx" in file_format):
-            from ..Format.Word import Word_Table
-            Word_Table(Dict_Temp, Location)
-        elif ("html" in file_format):
-            from ..Format.HTML import HTML_Table
-            HTML_Table(Dict_Temp, Location)
-        elif ("json" in file_format):
-            from ..Format.JSON import JSON_Table
-            JSON_Table(Dict_Temp, Location)
-        elif ("md" in file_format):
-            from ..Format.Markdown import Markdown_Table
-            Markdown_Table(Dict_Temp, Location)
-        elif ("pdf" in file_format):
-            from ..Format.PDF import Create_PDF
-            Word_Table(Dict_Temp, Location)
-            if (osname == 'nt'): Create_PDF(Location)
-            else: print("At this point it's not be possible to convert a docx file into a pdf under linux.\nPlease try it under windows.\n")
-        elif ("tex" in file_format):
-            from ..Format.LaTeX import Latex_Table
-            Latex_Table(Dict_Temp, Location)
-        elif ("xlsx" in file_format):
-            from ..Format.Excel import Excel_Table
-            Excel_Table(Dict_Temp, Location)
-        elif ("xml" in file_format):
-            from ..Format.XML import XML_Table
-            #XML_Table(Dict_Temp, Location)
-        elif ("yaml" in file_format):
-            from ..Format.YAML import YAML_Table
-            #YAML_Table(Dict_Temp, Location)
+        # Write_File_Format
+        file_format(Dict_Temp, Location)
 
     except (ConnectionError, gaierror, RequestException):
         pass
