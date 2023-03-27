3faf0aee24fb69a832d8180e9c3c1ea032294bb1
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-27 16:27:24 +0200

    Update JSON.py

--- Resources/Format/JSON.py
+++ Resources/Format/JSON.py
@@ -10,20 +10,34 @@ def JSON_Table(Dict_Result, location, Array_Files = []):
         with open(join(location, result_file), 'w', encoding='UTF-8') as f:
             json.dump(Dict_Result[keyword], f)
 
-    if (Dict_Header['Header'] != {}):
-        Array_Files.append(join(location, 'result_header.json'))
-        Write_JSON('Header', location, 'result_header.json')
-    if (Dict_Header['Information'] != {}):
-        Array_Files.append(join(location, 'result_information_disclosure.json'))
-        Write_JSON('Information', location, 'result_information_disclosure.json')
-    if (Dict_Header['SSH'] != {}):
-        Array_Files.append(join(location, 'result_ssh_vulns.json'))
-        Write_JSON('SSH', location, 'result_ssh_vulns.json')
-    if (Dict_Header['Security_Flag'] != {}):
-        Array_Files.append(join(location, 'result_security_flags.json'))
-        Write_JSON('Security_Flag', location, 'result_security_flags.json')
+    # Certificates
     if (Dict_Header['Certificate'] != {}):
         Array_Files.append(join(location, 'result_certificate.json'))
         Write_JSON('Header', location, 'result_certificate.json')
+
+    # Cookie_Flags
+    if (Dict_Header['Security_Flag'] != {}):
+        Array_Files.append(join(location, 'result_security_flags.json'))
+        Write_JSON('Security_Flag', location, 'result_security_flags.json')
+
+    # HTTP_Header
+    if (Dict_Header['Header'] != {}):
+        Array_Files.append(join(location, 'result_header.json'))
+        Write_JSON('Header', location, 'result_header.json')
+
+    # HTTP_Information_Disclosure
+    if (Dict_Header['Information'] != {}):
+        Array_Files.append(join(location, 'result_information_disclosure.json'))
+        Write_JSON('Information', location, 'result_information_disclosure.json')
+
+    # SSH
+    if (Dict_Header['SSH'] != {}):
+        Array_Files.append(join(location, 'result_ssh_vulns.json'))
+        Write_JSON('SSH', location, 'result_ssh_vulns.json')
+
+    # SSL
+    if (Dict_Result['SSL'] != {}):
+        Array_Files.append(join(location, 'result_ssl.json'))
+        Write_JSON('SSL', location, 'result_ssl.json')       
 
     return Array_Files
