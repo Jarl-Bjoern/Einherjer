490015497859f763322da5ea2bcb40f3d3396803
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-29 19:06:54 +0200

    Update CSV.py

--- Resources/Format/CSV.py
+++ Resources/Format/CSV.py
@@ -148,11 +148,12 @@ def CSV_Table(Dict_Result, location, Write_Mode = "", Write_Second_Mode = ""):
                 Array_Temp.append(Target)
                 for Result_Left, Result_Right in Dict_Result['HTTP_Methods'][Target].items():
                     if (Result_Left == "DNS" and Result_Right == ""):  Result_Right = "FEHLT"
-                    elif (Result_Left != "DNS" and Result_Right == "FEHLT"): Result_Right = "FEHLT"
+                    #elif (Result_Left != "DNS" and Result_Right == "FEHLT"): Result_Right = "FEHLT"
 
                     if (Result_Left != "DNS" and Result_Right == "FEHLT"): Array_Temp.append("✓")
-                    elif (Result_Left == "DNS" and Result_Right != "FEHLT"): Array_Temp.append("-")
-                    else: Array_Temp.append("X")
+                    elif (Result_Left == "DNS" and Result_Right == "FEHLT"): Array_Temp.append("-")
+                    elif (Result_Left == "DNS" and Result_Right != "FEHLT"): Array_Temp.append(Result_Right)
+                    elif (Result_Left != "DNS" and Result_Right != "FEHLT"): Array_Temp.append("X")
                 writer.writerow(Array_Temp)
 
     if (Dict_Result['SSL'] != {}):
