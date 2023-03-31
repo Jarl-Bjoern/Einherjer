a065e9b88e7b41d1692ab0dc7f8547a4153026f7
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-31 20:35:13 +0200

    Update Filter_Target_Split.py

--- Resources/Filter/Filter_Target_Split.py
+++ Resources/Filter/Filter_Target_Split.py
@@ -5,7 +5,7 @@
 # Libraries
 from ..Header_Files.Variables import *
 
-def Target_Split(input_file, output_location, Array_Output):
+def Target_Split(input_file, output_location, Array_Temp = []):
     try:
         with open(input_file, 'r') as f:
             Temp_Array = f.read().splitlines()
@@ -19,6 +19,10 @@ def Target_Split(input_file, output_location, Array_Output):
                 if (i == Number_Split):
                     for _ in range(Number_Split, len(Temp_Array)):
                         Target_File_B.write(f'{Temp_Array[_]}\n')
+                        Array_Temp.append(f'{output_location}/Targets_A.txt')
                     break
                 else:
                     Target_File_A.write(f'{Temp_Array[i]}\n')
+                    Array_Temp.append(f'{output_location}/Targets_B.txt')
+
+    return Array_Temp
