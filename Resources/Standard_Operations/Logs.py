707ada3acb8604188f52fce15e2c5e465b2c6c15
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-03-31 16:08:55 +0200

    Update Logs.py

--- Resources/Standard_Operations/Logs.py
+++ Resources/Standard_Operations/Logs.py
@@ -15,16 +15,25 @@ class Logs:
         with open(join(Log_Path, f'{Date}_tracefile.log'), 'a') as f:
             f.write(f'{Text}\n')
 
-    def Write_Log(url, host, Log_Path):
+    def Write_Log(url, host, Log_Path, Array_Temp = []):
         if (not exists(Log_Path)): makedirs(Log_Path)
-        with open(join(Log_Path, f'{Date}_failed-url.txt'), 'a') as f:
-            f.write(f'{url}\n')
+        else:
+            # Check_For_Duplicate
+            with open(join(Log_Path), f'{Date}_failed-url.txt', 'r') as f:
+                Array_Temp = f.read().splitlines()
+
+        # Write_Fail
+        if (url not in Array_Temp):
+            with open(join(Log_Path, f'{Date}_failed-url.txt'), 'a') as f:
+                f.write(f'{url}\n')
+
+        # Write_Fail_To_Global_Log
         if (host != ""):
             Logs.Log_File(Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.RED+'FAILED\n'+Colors.RESET, Log_Path)
         else:
             Logs.Log_File(Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '+Colors.RED+'FAILED\n'+Colors.RESET, Log_Path)
 
-    def Log_File(Text, Log_Path):
+    def Log_File(Text, Log_Path, Array_Temp = []):
         if (not exists(Log_Path)): makedirs(Log_Path)
         with open(join(Log_Path, f"{Date}.log"), "a") as f:
             f.write(Text)
