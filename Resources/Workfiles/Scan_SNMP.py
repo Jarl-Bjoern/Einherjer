b8c9fe05bba12a3fb6991ec9247196e28066f184
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-04-02 21:40:24 +0200

    Update Scan_SNMP.py

--- Resources/Workfiles/Scan_SNMP.py
+++ Resources/Workfiles/Scan_SNMP.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
+
+# Libraries
+
+class Check_SNMP:
+    def Basic_Check(url, Dict_Temp = {}):
+        pass
+
+    def String_Enumeration(url, Dict_Temp = {}):
+        pass
