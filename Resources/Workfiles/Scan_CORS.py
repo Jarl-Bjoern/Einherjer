b07969164a5d4b31c610c0f446b66579df118f64
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-04-04 20:27:38 +0200

    Update Scan_CORS.py

--- Resources/Workfiles/Scan_CORS.py
+++ Resources/Workfiles/Scan_CORS.py
@@ -3,6 +3,85 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
+from ..Header_Files.Variables import *
+from ..Standard_Operations.Logs import Logs
+from ..Standard_Operations.Colors import Colors
+from ..Standard_Operations.Standard import Standard
 
-def Test():
-    pass
+def Check_CORS_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Location, Allow_Redirects):
+    try
+        # Session_Creation
+        with Session() as s:
+
+            # Auth_Configuration
+            if (Dict_Auth['pkcs12_cert'] != ''):
+                s.mount(url, Pkcs12Adapter(pkcs12_filename=Dict_Auth['pkcs12_cert'], pkcs12_password=Dict_Auth['pkcs12_password']))
+                if (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != ''):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        headers={'Origin': "einherjer-test-scanning.vul"},
+                        verify=False,
+                        allow_redirects=Allow_Redirects,
+                        proxies=Dict_Proxies,
+                        pkcs12_filename=Dict_Auth['pkcs12_cert'],
+                        pkcs12_password=Dict_Auth['pkcs12_password']
+                    )
+
+                elif (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == ''):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        headers={'Origin': "einherjer-test-scanning.vul"},
+                        verify=False,
+                        allow_redirects=Allow_Redirects,
+                        pkcs12_filename=Dict_Auth['pkcs12_cert'],
+                        pkcs12_password=Dict_Auth['pkcs12_password']
+                    )
+            else:
+                if (Dict_Auth['user'] != '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        headers={'Origin': "einherjer-test-scanning.vul"},
+                        verify=False,
+                        allow_redirects=Allow_Redirects,
+                        proxies=Dict_Proxies,
+                        auth=(Dict_Auth['user'], Dict_Auth['password'])
+                    )
+
+                elif (Dict_Auth['user'] != '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        headers={'Origin': "einherjer-test-scanning.vul"},
+                        verify=False,
+                        allow_redirects=Allow_Redirects,
+                        auth=(Dict_Auth['user'], Dict_Auth['password'])
+                    )
+
+                elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] != '' or Dict_Proxies['https'] != '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        headers={'Origin': "einherjer-test-scanning.vul"},
+                        verify=False,
+                        allow_redirects=Allow_Redirects,
+                        proxies=Dict_Proxies
+                    )
+
+                elif (Dict_Auth['user'] == '' and (Dict_Proxies['http'] == '' and Dict_Proxies['https'] == '')):
+                    r = s.get(
+                        url,
+                        timeout=(t_seconds, t_seconds),
+                        headers={'Origin': "einherjer-test-scanning.vul"},
+                        verify=False,
+                        allow_redirects=Allow_Redirects
+                    )
+
+        # Terminate_Session
+        r.close()
+
+
+    except ReadTimeout:
+        Logs.Write_Log(url, Host_Name, join(Location, 'Logs'))
