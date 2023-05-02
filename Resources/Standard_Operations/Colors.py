751f97552e217094a2a20aa091f69d3cabf21df5
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:53:05 +0200

    Update Colors.py

--- Resources/Standard_Operations/Colors.py
+++ Resources/Standard_Operations/Colors.py
@@ -3,12 +3,12 @@
 # Rainer Christian Bjoern Herold
 
 class Colors:
-    CYAN = '\033[36m'
-    GREEN = '\033[32m'
-    ORANGE = '\033[33m'
-    BLUE = '\033[34m'
-    RED = '\033[31m'
-    YELLOW = '\033[33m'
-    PURPLE = '\033[35m'
+    CYAN      = '\033[36m'
+    GREEN     = '\033[32m'
+    ORANGE    = '\033[33m'
+    BLUE      = '\033[34m'
+    RED       = '\033[31m'
+    YELLOW    = '\033[33m'
+    PURPLE    = '\033[35m'
     UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
+    RESET     = '\033[0m'
