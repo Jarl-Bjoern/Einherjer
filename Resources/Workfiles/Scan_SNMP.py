888d1851b67304b6a8053a3460fc96cd8747bcc8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:57:54 +0200

    Update Scan_SNMP.py

--- Resources/Workfiles/Scan_SNMP.py
+++ Resources/Workfiles/Scan_SNMP.py
@@ -3,9 +3,9 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from ..Header_Files.Variables import *
-from ..Standard_Operations.Logs import Logs
-from ..Standard_Operations.Colors import Colors
+from ..Header_Files.Variables       import *
+from ..Standard_Operations.Logs     import Logs
+from ..Standard_Operations.Colors   import Colors
 from ..Standard_Operations.Standard import Standard
 
 class Check_SNMP:
