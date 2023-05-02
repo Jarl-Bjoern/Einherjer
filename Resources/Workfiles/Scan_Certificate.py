7573a6895d304d7a81106c3ad6e8e37f0bda81b5
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:12:48 +0200

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -31,55 +31,59 @@ def Check_Certificate(url, t_seconds, Host_Name, Location, context = create_unve
 
     try:
         with create_connection((Target, int(Port)), timeout=t_seconds) as sock:
-            with context.wrap_socket(sock, server_hostname=Target) as ssock:
-                # Cert_Connect_And_Collect
-                cert_der = ssock.getpeercert(binary_form=True)
-                try:
-                    cert = load_der_x509_certificate(cert_der, default_backend())
-                except TypeError:
-                    cert_der = ssock.getpeercert(binary_form=False)
-                    cert = load_der_x509_certificate(cert_der, default_backend())
+            try:
+                with context.wrap_socket(sock, server_hostname=Target) as ssock:
+                    # Cert_Connect_And_Collect
+                    cert_der = ssock.getpeercert(binary_form=True)
+                    try:
+                        cert = load_der_x509_certificate(cert_der, default_backend())
+                    except TypeError:
+                        cert_der = ssock.getpeercert(binary_form=False)
+                        cert = load_der_x509_certificate(cert_der, default_backend())
 
-                # Get_Cert_Information
-                Current_Date = datetime.now()
+                    # Get_Cert_Information
+                    Current_Date = datetime.now()
 
-                Dict_Temp['Issuer']              = str(cert.issuer)[6:-2]
-                Dict_Temp['Subject']             = str(cert.subject)[6:-2]
-                Dict_Temp['Signature_Algorithm'] = str(cert.signature_algorithm_oid).split('name=')[1][:-2].upper()
-                Dict_Temp['Public_Key']          = resplit('<| ', str(cert.public_key()))[1].split('.')[::-1][0]
-                Dict_Temp['Cert_Creation_Date']  = str(cert.not_valid_before)
-                Dict_Temp['Cert_EOL']            = str(cert.not_valid_after)
+                    Dict_Temp['Issuer']              = str(cert.issuer)[6:-2]
+                    Dict_Temp['Subject']             = str(cert.subject)[6:-2]
+                    Dict_Temp['Signature_Algorithm'] = str(cert.signature_algorithm_oid).split('name=')[1][:-2].upper()
+                    Dict_Temp['Public_Key']          = resplit('<| ', str(cert.public_key()))[1].split('.')[::-1][0]
+                    Dict_Temp['Cert_Creation_Date']  = str(cert.not_valid_before)
+                    Dict_Temp['Cert_EOL']            = str(cert.not_valid_after)
 
-                # Cert_Expire_Filter
-                Date_Block = str(cert.not_valid_after).split(' ')[0].split('-')
-                Date_Difference = (Current_Date - datetime(int(Date_Block[0]), int(Date_Block[1]), int(Date_Block[2]))).days
-                if (Date_Difference < 0): Dict_Temp['Date_Difference'] = f'{str(Date_Difference)[1:]} days before expires'
-                else:                     Dict_Temp['Date_Difference'] = f'expired since {Date_Difference} days'
+                    # Cert_Expire_Filter
+                    Date_Block = str(cert.not_valid_after).split(' ')[0].split('-')
+                    Date_Difference = (Current_Date - datetime(int(Date_Block[0]), int(Date_Block[1]), int(Date_Block[2]))).days
+                    if (Date_Difference < 0): Dict_Temp['Date_Difference'] = f'{str(Date_Difference)[1:]} days before expires'
+                    else:                     Dict_Temp['Date_Difference'] = f'expired since {Date_Difference} days'
 
-                # Add_Scan_Date
-                Dict_Temp['Current_Date'] = str(Current_Date).split('.')[0]
+                    # Add_Scan_Date
+                    Dict_Temp['Current_Date'] = str(Current_Date).split('.')[0]
 
-        # Logging
-        if (Host_Name != ""):
-            Logs.Log_File(
-                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-                +Colors.BLUE+'Certificate-Check\n'
-                +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-                +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - {Host_Name} - '+Colors.CYAN
-                +'Certificate Information was succesfully recorded.\n\n',
-                join(Location, 'Logs')
-            )
-            Standard.Write_Output_File('affected_certificate_targets.txt', f'{url} ({Host_Name})', Location)
-        else:
-            Logs.Log_File(
-                Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-                +Colors.BLUE+'Certificate-Check\n'
-                +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
-                +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '
-                +Colors.CYAN+'Certificate Information was successfully recorded.\n\n',
-                join(Location, 'Logs')
-            )
-            Standard.Write_Output_File('affected_certificate_targets.txt', f'{url} (-)', Location)
+                    # Logging
+                    if (Host_Name != ""):
+                        Logs.Log_File(
+                            Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                            +Colors.BLUE+'Certificate-Check\n'
+                            +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                            +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - {Host_Name} - '+Colors.CYAN
+                            +'Certificate Information was succesfully recorded.\n\n',
+                            join(Location, 'Logs')
+                        )
+                        Standard.Write_Output_File('affected_certificate_targets.txt', f'{url} ({Host_Name})', Location)
+                    else:
+                        Logs.Log_File(
+                            Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                            +Colors.BLUE+'Certificate-Check\n'
+                            +Colors.YELLOW+'-----------------------------------------------------------------------------------------------------------\n'
+                            +Colors.GREEN+f'{strftime("%Y-%m-%d %H:%M:%S")}'+Colors.RESET+f' - {url} - '
+                            +Colors.CYAN+'Certificate Information was successfully recorded.\n\n',
+                            join(Location, 'Logs')
+                        )
+                        Standard.Write_Output_File('affected_certificate_targets.txt', f'{url} (-)', Location)
+
+            except (ConnectionRefusedError, gaierror, SSLError, SSLZeroReturnError, TimeoutError):
+                Logs.Write_Log(url, Host_Name, join(Location, 'Logs'))
 
     except (ConnectionRefusedError, gaierror, SSLError, SSLZeroReturnError, TimeoutError):
         Logs.Write_Log(url, Host_Name, join(Location, 'Logs'))
