6c07420c08201a875ac1d13755c8fde8faa0d429
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:56:05 +0200

    Update Scan_Certificate.py

--- Resources/Workfiles/Scan_Certificate.py
+++ Resources/Workfiles/Scan_Certificate.py
@@ -3,9 +3,9 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from ..Header_Files.Variables import *
-from ..Standard_Operations.Logs import Logs
-from ..Standard_Operations.Colors import Colors
+from ..Header_Files.Variables       import *
+from ..Standard_Operations.Logs     import Logs
+from ..Standard_Operations.Colors   import Colors
 from ..Standard_Operations.Standard import Standard
 
 def Check_Certificate(url, t_seconds, Host_Name, Location, context = create_unverified_context(), Dict_Temp = {}):
