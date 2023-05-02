d884af9c0e109944f140aa4345d315c0063116b1
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-02 21:55:54 +0200

    Update Scan_CORS.py

--- Resources/Workfiles/Scan_CORS.py
+++ Resources/Workfiles/Scan_CORS.py
@@ -3,9 +3,9 @@
 # Rainer Christian Bjoern Herold
 
 # Libraries
-from ..Header_Files.Variables import *
-from ..Standard_Operations.Logs import Logs
-from ..Standard_Operations.Colors import Colors
+from ..Header_Files.Variables       import *
+from ..Standard_Operations.Logs     import Logs
+from ..Standard_Operations.Colors   import Colors
 from ..Standard_Operations.Standard import Standard
 
 def Check_CORS_Header(url, t_seconds, Host_Name, Dict_Proxies, Dict_Auth, Location, Allow_Redirects):
