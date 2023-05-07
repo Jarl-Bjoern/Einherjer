4ef84a6bf88503cd5dab8074e1595cc5e038d9ff
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-05-07 20:46:24 +0200

    Update Scan_Host_Name.py

--- Resources/Workfiles/Scan_Host_Name.py
+++ Resources/Workfiles/Scan_Host_Name.py
@@ -7,11 +7,11 @@ from ..Header_Files.Variables import *
 
 def Host_Swap(Word, T_Switch = ""):
     T_Switch = Word[:-2]
-    Word = T_Switch
+    Word     = T_Switch
     return Word
 
 def Filter_Host_Name(Element, Target, Array_Temp, Word):
-    if (Element != Array_Temp[len(Array_Temp)-1] and Element != Target): Word += f"{Element}, "
+    if (Element != Array_Temp[len(Array_Temp)-1] and Element != Target):   Word += f"{Element}, "
     elif (Element != Array_Temp[len(Array_Temp)-1] and Element == Target): pass
     elif (Element == Array_Temp[len(Array_Temp)-1] and Element == Target):
         if (Word[-2:] == ", "): Word = Host_Swap(Word)
@@ -22,8 +22,8 @@ def Filter_Host_Name(Element, Target, Array_Temp, Word):
 def Get_Host_Name(url, Target = "", Temp = "", Word = ""):
     if ('//' in url):
         if (url.count(':') == 2): Target = url.split('//')[1].split(':')[0]
-        else: Target = url.split('//')[1]
-    else: Target = url
+        else:                     Target = url.split('//')[1]
+    else:                         Target = url
 
     try: Temp = gethostbyaddr(Target)
     except (gaierror, herror):
