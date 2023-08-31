fefcfba38deda10e85dc519e6837b2b83549e04c
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-08-31 20:16:54 +0200

    Update Filter_SSH_NMAP.py

--- Resources/Filter/Filter_SSH_NMAP.py
+++ Resources/Filter/Filter_SSH_NMAP.py
@@ -78,7 +78,8 @@ def SSH_Nmap(nmap_files_location, output_location, Dict_System = {}, Dict_SSH_Re
                         elif ("MAC Address:"     in Report[Result]   or
                               "Nmap done"        in Report[Result+1] or
                               "Nmap scan report" in Report[Result+1]):
-                                   Dict_System[f'{IP_Address}:{Port}'] = Dict_SSH_Results
+                                   try:    Dict_System[f'{IP_Address}:{Port}'] = Dict_SSH_Results
+                                   except: pass
                                    Dict_SSH_Results = {'kex_algorithms': [], 'server_host_key_algorithms': [], 'encryption_algorithms': [], 'mac_algorithms': [], 'auth_methods': []}
 
 
@@ -158,7 +159,8 @@ def SSH_Nmap(nmap_files_location, output_location, Dict_System = {}, Dict_SSH_Re
                         elif ("MAC Address:"     in Report[Result]   or
                               "Nmap done"        in Report[Result+1] or
                               "Nmap scan report" in Report[Result+1]):
-                                   Dict_System[f'{IP_Address}:{Port}'] = Dict_SSH_Results
+                                   try:    Dict_System[f'{IP_Address}:{Port}'] = Dict_SSH_Results
+                                   except: pass
                                    Dict_SSH_Results = {'kex_algorithms': [], 'server_host_key_algorithms': [], 'encryption_algorithms': [], 'mac_algorithms': [], 'auth_methods': []}
     
             Array_Temp.append(join(output_location, 'ssh-vulns.csv'))
