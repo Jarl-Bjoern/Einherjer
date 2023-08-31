473ff94907e86a1b3486bcbd2a3cd9b8268bc8e8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-08-31 22:10:21 +0200

    Create ArgParser_Fuzzing_Intro.py

---
+++ Resources/Header_Files/ArgParser_Fuzzing_Intro.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Libraries
+from Resources.Standard_Operations.Colors import Colors
+from .Libraries import dirname, join, realpath
+
+# Functions
+def Argument_Parser(Error_Text, Template_Location = dirname(realpath(__file__)).replace('Resources/Header_Files', 'Templates')):
+    from argparse import ArgumentParser, RawTextHelpFormatter, SUPPRESS
+
+    Program_Description = """-------------------------------------------------------------------------------------
+|  Rainer Christian Bjoern Herold                                                   |
+|  Copyright 2022-2023. All rights reserved.                                        |
+|                                                                                   |
+|  Please do not use the program for illegal activities.                            |
+|                                                                                   |
+|  If you got any problems don't hesitate to contact me so I can try to fix them.   |
+-------------------------------------------------------------------------------------
+"""
+
+    parser = ArgumentParser(add_help=False, formatter_class=RawTextHelpFormatter, description=Colors.ORANGE+Program_Description+Colors.RESET, allow_abbrev=False, usage=SUPPRESS)
+    fuzzing_arguments   = parser.add_argument_group(Colors.ORANGE+'brute-force arguments'+Colors.RESET)
+    optional          = parser.add_argument_group(Colors.ORANGE+'optional arguments'+Colors.RESET)
+
+    #brute_arguments.add_argument('-bDNS', '--brute-dns', type=str, help=Colors.GREEN+'UNDER CONSTRUCTION'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    #brute_arguments.add_argument('-bF', '--brute-fuzz', type=str, help=Colors.GREEN+'UNDER CONSTRUCTION'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    #brute_arguments.add_argument('-bFTP', '--brute-ftp', type=str, help=Colors.GREEN+'UNDER CONSTRUCTION'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    #brute_arguments.add_argument('-bSNMP', '--brute-snmp', type=str, help=Colors.GREEN+'UNDER CONSTRUCTION'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+
+    optional.add_argument('-h','--help', action='help', default=SUPPRESS, help=Colors.GREEN+'Show this help message and exit.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+
+    del ArgumentParser, RawTextHelpFormatter, SUPPRESS
+    parser.print_help()
+
+    print (Colors.RED+Error_Text+Colors.RESET)
