05ae56eff8e186e393f1c7eb4e341ce5022eb6d8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-08-31 21:51:26 +0200

    Update Scan_Screen.py

--- Resources/Workfiles/Scan_Screen.py
+++ Resources/Workfiles/Scan_Screen.py
@@ -12,7 +12,7 @@ class Web:
         if (osname == 'nt'): Chrome_Driver_Location = join(dirname(realpath(__file__)).split('Workfiles')[0], 'Webdriver/chromedriver.exe')
         else:                Chrome_Driver_Location = '/usr/bin/chromedriver'
         #return webdriver.Chrome(service=Service(Chrome_Driver_Location), options=options, executable_path=Chrome_Driver_Location)
-        #return webdriver.Chrome(service=Service(Chrome_Driver_Location), options=options)
+        return webdriver.Chrome(service=Service(Chrome_Driver_Location), options=options)
 
     def Configurate_Driver(options, driver = None):
         try: driver = Web.Driver_Specification(options)
