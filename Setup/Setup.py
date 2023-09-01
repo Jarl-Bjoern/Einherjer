67914ecc5762105fdd6449c7a0a9e64409e6661e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-09-01 21:48:25 +0200

    Update Setup.py

--- Setup/Setup.py
+++ Setup/Setup.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
+
+import setuptools
+
+# UNDER_CONSTRUCTION
+setuptools.setup(
+    include_package_data=True,
+    name='Einherjer',
+    version='0.9.0',
+    description='',
+    url='',
+    author='Jarl-Bjoern',
+    packages=setuptools.find_packages(),
+    install_requires=['','']
+    long_description='',
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
