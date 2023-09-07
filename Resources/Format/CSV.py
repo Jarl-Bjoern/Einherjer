4871ac09644b3f3c430c8a8a442a17cc057fcc87
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-09-07 20:09:09 +0200

    Update CSV.py

--- Resources/Format/CSV.py
+++ Resources/Format/CSV.py
@@ -34,8 +34,9 @@ def CSV_Table(Dict_Result, location, Write_Mode = "", Write_Second_Mode = ""):
                 if (Array_Temp.count('✓') != len(Dict_Header)):
                     writer.writerow(Array_Temp)
                 else:
+                    print (Target)
                     Standard.Remove_From_Filtered_File(join(location, 'affected_header_targets.txt'), Target)
-        Standard.Remove_Empty_Filter_File(join(location, 'result_header.csv'))
+        Standard.Remove_Empty_Filter_File(join(location, 'result_header.csv')), Standard.Remove_Empty_Filter_File(join(location, 'affected_header_targets.txt'))
 
     if (Dict_Result['Information'] != {}):
         # Check_For_Existing_File
