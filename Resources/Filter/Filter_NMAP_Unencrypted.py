37fc9f737241149d00b60b3b58bb225ae54da77d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-10-01 17:47:47 +0200

    Update Filter_NMAP_Unencrypted.py

--- Resources/Filter/Filter_NMAP_Unencrypted.py
+++ Resources/Filter/Filter_NMAP_Unencrypted.py
@@ -6,44 +6,61 @@
 from ..Header_Files.Variables import *
 
 def NMAP_Unencrypted(file_path, Array_Filter_Services  = ["access denied", ""], Array_Filter_Protocols = ['ssl','ldaps','https']):
-        Protocol, Address, Port, Skip_Attributes = "","","",False
+
         try:
-            for event, elem in ET.iterparse(file_path, events=("end",)):
-                if (event == "end"):
-                    if (elem.tag == "address"):
-                        if (Skip_Attributes != True):
-                            if (elem.attrib['addrtype'] == "ipv4"):
-                                Address = elem.attrib['addr']
 
-                    elif (elem.tag == "state"):
-                        if (elem.attrib['state'] != "open"):
-                            Skip_Attributes = True
+                Protocol, Address, Port, Skip_Attributes = "","","",False
+                try:
+                    for event, elem in ET.iterparse(file_path, events=("end",)):
+                        if (event == "end"):
+                            if (elem.tag == "address"):
+                                if (Skip_Attributes != True):
+                                    if (elem.attrib['addrtype'] == "ipv4"):
+                                        Address = elem.attrib['addr']
+        
+                            elif (elem.tag == "state"):
+                                if (elem.attrib['state'] != "open"):
+                                    Skip_Attributes = True
+        
+                            elif (elem.tag == "service"):
+                                if (Skip_Attributes != True):
+                                    Protocol = elem.attrib['name']
+        
+                                    Product, Version, Extra_Info = "","",""
+                                    try:             Product    = elem.attrib['product']
+                                    except KeyError: pass
+                                    try:             Version    = elem.attrib['version']
+                                    except KeyError: pass
+                                    try:             Extra_Info = elem.attrib['extrainfo']
+                                    except KeyError: pass
+        
+                                    Word = ""
+                                    if (Product    != ""): Word += f"{Product} "
+                                    if (Version    != ""): Word += f"{Version} "
+                                    if (Extra_Info != ""): Word += f"{Extra_Info}"
+        
+                            elif (elem.tag == 'port'):
+                                Port = elem.attrib['portid']
+        
+                                if (Protocol != "" and Address != "" and Port != ""):
+                                   if (Protocol not in Array_Filter_Protocols):
+                                       print (f'{Address} {Protocol} open {Word}')
+        
+                                Skip_Attributes = False
+        
+                except ET.ParseError:
+                    pass #print ("It's seems that the xml file"+Colors.RED+f" {file_path} "+Colors.RESET+"is empty."), exit()
 
-                    elif (elem.tag == "service"):
-                        if (Skip_Attributes != True):
-                            Protocol = elem.attrib['name']
-
-                            Product, Version, Extra_Info = "","",""
-                            try:             Product    = elem.attrib['product']
-                            except KeyError: pass
-                            try:             Version    = elem.attrib['version']
-                            except KeyError: pass
-                            try:             Extra_Info = elem.attrib['extrainfo']
-                            except KeyError: pass
-
-                            Word = ""
-                            if (Product    != ""): Word += f"{Product} "
-                            if (Version    != ""): Word += f"{Version} "
-                            if (Extra_Info != ""): Word += f"{Extra_Info}"
-
-                    elif (elem.tag == 'port'):
-                        Port = elem.attrib['portid']
-
-                        if (Protocol != "" and Address != "" and Port != ""):
-                           if (Protocol not in Array_Filter_Protocols):
-                               print (f'{Address} {Protocol} open {Word}')
-
-                        Skip_Attributes = False
-
-        except ET.ParseError:
-            pass #print ("It's seems that the xml file"+Colors.RED+f" {file_path} "+Colors.RESET+"is empty."), exit()
+            Array_Temp.append(join(output_location, 'ssh-vulns.csv'))
+            with open(join(output_location, 'ssh-vulns.csv'), 'w') as f:
+                 f.write("Host;kex_algorithms;server_host_key_algorithms;encryption_algorithms;mac_algorithms;auth_methods\n")
+                 for i in Dict_System:
+                     f.write(f'{i};')
+                     for j in Dict_System[i]:
+                         for k in range(0, len(Dict_System[i][j])):
+                             if (k != len(Dict_System[i][j])-1): f.write(f'{Dict_System[i][j][k]}, ')
+                             else: f.write(f'{Dict_System[i][j][k]}')
+                         f.write(f';')
+                     f.write('\n')
+    except FileNotFoundError:
+        pass
