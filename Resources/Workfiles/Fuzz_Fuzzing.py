63195c3c911d0af25ac9838f41e93b6952ea76d7
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-11-12 16:19:07 +0100

    Update Fuzz_Fuzzing.py

--- Resources/Workfiles/Fuzz_Fuzzing.py
+++ Resources/Workfiles/Fuzz_Fuzzing.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables import *
 from ..Standard_Operations.Logs import Logs
 from ..Standard_Operations.Colors import Colors
 
-def Check_Site_Paths(url, t_seconds, array_wordlists, Dict_Result = {"200": [], "204": [], "301": [], "302": [], "307": [], "308": [], "401": [], "403": [], "405": [], "500": []}, Array_Temp = [], Array_Status_Code = ["200", "204", "301", "302", "307", "308", "401", "403", "405", "500"]):
+def Check_Site_Paths(url, t_seconds, Host_Name, array_wordlists, Dict_Result = {"200": [], "204": [], "301": [], "302": [], "307": [], "308": [], "401": [], "403": [], "405": [], "500": []}, Array_Temp = [], Array_Status_Code = ["200", "204", "301", "302", "307", "308", "401", "403", "405", "500"]):
     async def Check_Fuzz(url):
         Limit = TCPConnector(limit_per_host=100)
         async with ClientSession(connector=Limit, trust_env=True) as s:
