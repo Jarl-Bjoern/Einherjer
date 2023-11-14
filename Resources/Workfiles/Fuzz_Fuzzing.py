d01b43b678444a222557cf65f8df4afba76da2b1
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-11-14 23:39:55 +0100

    Update Fuzz_Fuzzing.py

--- Resources/Workfiles/Fuzz_Fuzzing.py
+++ Resources/Workfiles/Fuzz_Fuzzing.py
@@ -9,14 +9,14 @@ from ..Standard_Operations.Colors import Colors
 
 def Check_Site_Paths(url, t_seconds, Host_Name, array_wordlists, Location, Dict_Result = {"200": [], "204": [], "301": [], "302": [], "307": [], "308": [], "401": [], "403": [], "405": [], "500": []}, Array_Temp = [], Array_Status_Code = ["200", "204", "301", "302", "307", "308", "401", "403", "405", "500"]):
     async def Check_Fuzz(url):
-        Limit = TCPConnector(limit_per_host=100)
+        Limit, Client_Timeout = TCPConnector(limit_per_host=100), ClientTimeout(total=30)
         async with ClientSession(connector=Limit, trust_env=True) as s:
             for Word in array(array_wordlists):
                 URL = f'{url}/{Word}'
-                async with s.get(html_encode(URL), ssl=False) as r:
+                async with s.get(url_encode(URL), ssl=False, timeout=Client_Timeout) as r:
                     if (str(r.status) in Array_Status_Code):
                         if (URL not in Array_Temp):
-                            Array_Temp.append(html_encode(URL))
+                            Array_Temp.append(url_encode(URL))
                             Dict_Result[str(r.status)].append(URL)
                     await asyncio.sleep(t_seconds)
 
