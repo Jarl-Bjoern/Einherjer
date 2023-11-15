79f50f4367e3e1923c77eb84f308aa519490e588
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-11-15 18:37:46 +0100

    Update Fuzz_Fuzzing.py

--- Resources/Workfiles/Fuzz_Fuzzing.py
+++ Resources/Workfiles/Fuzz_Fuzzing.py
@@ -12,12 +12,28 @@ def Check_Site_Paths(url, t_seconds, Host_Name, array_wordlists, Location, Dict_
         Limit, Client_Timeout = TCPConnector(limit_per_host=100), ClientTimeout(total=30)
         Client_Headers = {'Connection': 'close'}
         async with ClientSession(connector=Limit, trust_env=True, headers=Client_Headers) as s:
+            # Get_Protocol
+            if ('http://' in url):
+                Protocol, Begin =  'http://', 7
+            elif ('https://' in url):
+                Protocol, Begin = 'https://', 8
+
             for Word in array(array_wordlists):
-                URL = f'{url}/{Word}'
+                # Convert_With_URL_Encoding
+                if (url.count('/') >= 3 and '//' in url):
+                    Temp_URL_Switcher = url_encode(url[Begin:])
+                    url               = f'{Protocol}{Temp_URL_Switcher.replace("2%F", "/")}'
+                elif (url.count('/') == 2 and '//' in url):
+                    pass
+
+                # Generate_URL_Encoded_Word
+                URL = f'{url}/{url_encode(Word).replace("2%F", "/")}'
+
+                # Start_Fuzz
                 async with s.get(url_encode(URL), ssl=False, timeout=Client_Timeout) as r:
                     if (str(r.status) in Array_Status_Code):
                         if (URL not in Array_Temp):
-                            Array_Temp.append(url_encode(URL))
+                            Array_Temp.append(URL)
                             Dict_Result[str(r.status)].append(URL)
                     await asyncio.sleep(t_seconds)
 
