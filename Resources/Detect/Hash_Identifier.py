e1b0ef29c320ee36b52f2716614bc04351b236a7
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-11-26 18:23:19 +0100

    Update Hash_Identifier.py

--- Resources/Detect/Hash_Identifier.py
+++ Resources/Detect/Hash_Identifier.py
@@ -8,12 +8,12 @@ from ..Header_Files.Variables import *
 def Get_Hash(Hash_Location, output_location, Array_Hashes = []):
     # Read_Files
     if (type(Hash_Location) == str):
-        with open(Hash_Location, encoding='utf-8', 'r') as f:
+        with open(Hash_Location, 'r', encoding='utf-8') as f:
             Array_Hashes = f.read().splitlines()
     elif (type(Hash_Location) == list)
         for _ in listdir(Hash_Location):
             Array_Temp = []
-            with open(join(Hash_Location, _), encoding='utf-8', 'r') as f:
+            with open(join(Hash_Location, _), 'r', encoding='utf-8') as f:
                 Array_Temp = f.read().splitlines()
             for _ in array(Array_Temp):
                 if (_ not in Array_Hashes):
