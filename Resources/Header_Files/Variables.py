94edd6657b753eeb6a364d1a9616bacd33c64c4b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-11-26 16:59:42 +0100

    Update Variables.py

--- Resources/Header_Files/Variables.py
+++ Resources/Header_Files/Variables.py
@@ -25,7 +25,6 @@ if (Program_Mode == "Scanning_Mode"):
 
     if (args.read_custom_header_file != False):
         Dict_Custom_Header = Standard.Read_JSON_File(dirname(realpath(__file__)).replace('Resources/Header_Files', "Config/http_custom_header.json"))
-        print (Dict_Custom_Header), exit()
     else:
         Dict_Custom_Header = {"Connection": "Close"}
 
