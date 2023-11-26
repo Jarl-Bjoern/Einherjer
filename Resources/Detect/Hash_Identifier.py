5b781a7a7361d4aeb9a24d22a50ffeb202630af9
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-11-26 18:23:38 +0100

    Update Hash_Identifier.py

--- Resources/Detect/Hash_Identifier.py
+++ Resources/Detect/Hash_Identifier.py
@@ -10,7 +10,7 @@ def Get_Hash(Hash_Location, output_location, Array_Hashes = []):
     if (type(Hash_Location) == str):
         with open(Hash_Location, 'r', encoding='utf-8') as f:
             Array_Hashes = f.read().splitlines()
-    elif (type(Hash_Location) == list)
+    elif (type(Hash_Location) == list):
         for _ in listdir(Hash_Location):
             Array_Temp = []
             with open(join(Hash_Location, _), 'r', encoding='utf-8') as f:
