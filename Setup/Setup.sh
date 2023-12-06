136eaea237479d97f1e3d07bebaac281918e9443
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-06 22:02:19 +0100

    Update Setup.sh

--- Setup/Setup.sh
+++ Setup/Setup.sh
@@ -6,12 +6,17 @@ TEMP_PATH=$(readlink -f -- "$0")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-6}
 
+# Virtual_Environment
 python3 -m virtualenv "$SCRIPT_PATH/venv"
 source "$SCRIPT_PATH/venv/bin/activate"
 pip3 install -r "$SCRIPT_PATH/Setup/requirements.txt"
 deactivate
 
+# Install_Missing_Global_Packages
 for LINE in $(cat "$SCRIPT_PATH/Setup/requirements.txt");
 do
   pip3 install $LINE || return 0
 done
+
+# Install_Missing_APT_Packages
+sudo apt install -y libcurl4-openssl-dev
