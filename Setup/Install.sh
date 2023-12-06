fa25a8250e35a8bc5e071388514f7ac1ed30eb1a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-06 21:53:54 +0100

    Update Install.sh

--- Setup/Install.sh
+++ Setup/Install.sh
@@ -4,7 +4,7 @@
 # Variables
 TEMP_PATH=$(readlink -f -- "$0")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
-SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-13}
+SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-16}
 BASE_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-23}
 
 echo $SCRIPT_PATH
