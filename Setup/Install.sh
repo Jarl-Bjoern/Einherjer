ac73692280a7a7b3e577a8a104ff3317c012bd91
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-06 21:49:45 +0100

    Create Install.sh

---
+++ Setup/Install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+TEMP_PATH=$(readlink -f -- "$0")
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-13}
+BASE_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-23}
+
+python3 -m virtualenv $BASE_PATH/venv
+source $BASE_PATH/venv/bin/activate
+pip3 install -r Setup/requirements.txt
+deactivate
+
+pip3 install -r Setup/requirements.txt
