76668f693b265f0b87c21fcabc11ae574863d715
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2023-12-06 21:55:40 +0100

    Update Install.sh

--- Setup/Install.sh
+++ Setup/Install.sh
@@ -6,12 +6,9 @@ TEMP_PATH=$(readlink -f -- "$0")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 SCRIPT_PATH=${TEMP_PATH::-${#SCRIPT_NAME}-6}
 
-echo $SCRIPT_PATH
-exit
-
 python3 -m virtualenv $SCRIPT_PATH/venv
 source $SCRIPT_PATH/venv/bin/activate
-pip3 install -r Setup/requirements.txt
+pip3 install -r $SCRIPT_PATH/Setup/requirements.txt
 deactivate
 
-pip3 install -r Setup/requirements.txt
+pip3 install -r $SCRIPT_PATH/Setup/requirements.txt
