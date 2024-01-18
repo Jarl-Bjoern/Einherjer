582bc1cfe059dba4ab5bf91e654f051eacfa8fde
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-01-18 19:56:38 +0100

    Update Scan_Screen.py

--- Resources/Workfiles/Scan_Screen.py
+++ Resources/Workfiles/Scan_Screen.py
@@ -51,7 +51,7 @@ class Web:
                         pass
         return Temp_Output_File
 
-def Take_Screenshot(url, driver_options, driver_path, Screen_Dir, switch_internet_connection, screenshot_wait, webdriver_timeout, screenshot_frame_thickness):
+def Take_Screenshot(url, driver_options, driver_path, Screen_Dir, switch_internet_connection, screenshot_wait, webdriver_timeout, screenshot_frame_thickness, screen_frame_switch):
     if (switch_internet_connection == True):
         if (osname == 'nt'):
             Chrome_Path = ChromeDriverManager().install()
@@ -101,12 +101,13 @@ def Take_Screenshot(url, driver_options, driver_path, Screen_Dir, switch_interne
     finally:
         driver.quit()
 
-    for Picture in listdir(Screen_Dir):
-        raw_image              = imread(join(Screen_Dir, Picture))
-        height                 = raw_image.shape[0]
-        width                  = raw_image.shape[1]
-        start_point, end_point = (-1,-1), (width, height)
-        color                  = Screenshot_Color
-        thickness              = screenshot_frame_thickness
-        img                    = rectangle(raw_image, start_point, end_point, color, thickness)
-        imwrite(join(Screen_Dir, Picture), img)
+    if (screen_frame_switch == True):
+        for Picture in listdir(Screen_Dir):
+            raw_image              = imread(join(Screen_Dir, Picture))
+            height                 = raw_image.shape[0]
+            width                  = raw_image.shape[1]
+            start_point, end_point = (-1,-1), (width, height)
+            color                  = Screenshot_Color
+            thickness              = screenshot_frame_thickness
+            img                    = rectangle(raw_image, start_point, end_point, color, thickness)
+            imwrite(join(Screen_Dir, Picture), img)
