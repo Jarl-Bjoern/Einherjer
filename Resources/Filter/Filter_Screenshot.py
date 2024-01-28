e299b7c33399bff9dabc8a2095c63c3b95abc907
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-01-28 12:23:44 +0100

    Update Filter_Screenshot.py

--- Resources/Filter/Filter_Screenshot.py
+++ Resources/Filter/Filter_Screenshot.py
@@ -18,7 +18,7 @@ def Screenshot_Frame(Screen_Dir, Screenshot_Thickness, Array_Temp = []):
                             copy2(join(Screen_Dir, Picture), join(Screen_Dir, 'Einherjer_Screenshot_Backup'))
                             break
                         except PermissionError:
-                            input (f"It seems that the path {Screen_Dir} is not writeable. Please change the permissions and try it again.\n\nPress 'Return' to continue.")
+                            input (f"\nIt seems that the path {Screen_Dir} is not writeable. Please change the permissions and try it again.\n\nPress 'Return' to continue.")
                             Standard.Print_Header()
 
                     raw_image              = imread(join(Screen_Dir, Picture))
