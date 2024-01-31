551c7025b87c4f1ad600991bee6fe53547bab031
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-01-31 17:58:31 +0100

    Update Libraries.py

--- Resources/Header_Files/Libraries.py
+++ Resources/Header_Files/Libraries.py
@@ -46,7 +46,7 @@ try:
     from rich.progress       import BarColumn, Progress, SpinnerColumn, TaskProgressColumn, TimeElapsedColumn, TimeRemainingColumn
     from selenium.common.exceptions import *
     from socket              import gaierror, gethostbyaddr, gethostbyname, herror, setdefaulttimeout as socket_defaulttimeout
-    from ssl                 import cert_time_to_seconds, create_default_context, _create_unverified_context as create_unverified_context, get_server_certificate, SSLError, SSLZeroReturnError
+    from ssl                 import cert_time_to_seconds, create_default_context, _create_unverified_context as create_unverified_context, get_server_certificate, SSLCertVerificationError, SSLError, SSLZeroReturnError
     from subprocess          import getoutput, run
     from stdiomask           import getpass
     from secrets             import SystemRandom
