2c263e7ad8a40014ae405e6df52d72e910590ffd
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-02-20 18:11:42 +0100

    Update Filter_NMAP_SSH.py

--- Resources/Filter/Filter_NMAP_SSH.py
+++ Resources/Filter/Filter_NMAP_SSH.py
@@ -211,7 +211,7 @@ def SSH_Nmap(nmap_files_location, output_location, Dict_System = {}, Dict_SSH_Re
         with open(join(output_location, 'ssh-vulns.csv'), 'w') as fw:
             with open(join(output_location, 'ssh-vulns-temp.csv'), 'r') as f:
                 for _ in f.read().splitlines():
-                    if (';;;;;;' not in _):
+                    if (';;;;;;' not in _ and _.count(';') == 1):
                         fw.write(f'{_}\n')
 
         remove(join(output_location, 'ssh-vulns-temp.csv'))
