4f6f22116535b583eed39051764a23be94b250fb
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-19 18:08:19 +0200

    Update main.py

--- Resources/main.py
+++ Resources/main.py
@@ -840,5 +840,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         if (len(Dict_State['State']) > 0):
             Standard.Write_State_File(Dict_State['State'], Dict_State['Location'])
-            print(Colors.ORANGE+f'\n\n\t\t\tA State file was written to continue the scan the next time!\n\n\t\t\t\t    Location: {join(Dict_State["Location"], "scan.state")}\n\n\t\t\t\t      The program will now be closed.\n\n'+Colors.RESET)
+            print(Colors.ORANGE+f'\n\n\t\t\tA State file was written to continue the scan the next time!\n\n\t\t\t\t    Location: '+Colors.CYAN+f'{join(Dict_State["Location"], "scan.state")}\n\n\t\t\t\t      '+Colors.ORANGE+f'The program will now be closed.\n\n'+Colors.RESET)
         exit()
