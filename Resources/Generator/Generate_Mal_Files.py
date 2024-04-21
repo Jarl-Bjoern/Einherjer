4122bdc280a133383c41fcd6df68937816de58d9
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-21 11:29:47 +0200

    Update Generate_Mal_Files.py

--- Resources/Generator/Generate_Mal_Files.py
+++ Resources/Generator/Generate_Mal_Files.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 # Rainer Christian Bjoern Herold
 
-def Generate_Files(Mode, target_server, output_location):
+def Generate_Files(mode, target_server, file_name, output_location):
     pass
