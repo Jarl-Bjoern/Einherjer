5dfa07deaa66cb738a8f7e9436c4e924c27ec929
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-04-22 21:46:02 +0200

    Update Filter_Nessus.py

--- Resources/Filter/Filter_Nessus.py
+++ Resources/Filter/Filter_Nessus.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables import *
 
 def Nessus_Reader(nessus_file_location, output_location, Array_Temp = []):
   # <preference><name>TARGET</name><value> --> Host | <plugin_output> Installed version | plugin_name (see_also)
-  if (isfile(nessus_file_location)):
+  if (isfile(nessus_file_location) and nessus_file_location.endswith('.nessus')):
       with open(nessus_file_location, 'r') as f:
           Report = f.read().splitlines()
 
