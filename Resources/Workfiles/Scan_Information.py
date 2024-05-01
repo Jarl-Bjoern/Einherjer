9e2d044aaadf0cb27f764204c9882f889c784fb4
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-05-01 19:31:23 +0200

    Update Scan_Information.py

--- Resources/Workfiles/Scan_Information.py
+++ Resources/Workfiles/Scan_Information.py
@@ -13,16 +13,16 @@ class Get_Information:
         Array_Filter = ["Apache/", "Tomcat/", "Server Version:"]
 
         with open('/opt/test.txt', 'w') as f:
-             for i in array(Read_File(argv[1])):
+             for i in np_array(Read_File(argv[1])):
                  r = get(str(i), verify=False, timeout=(25,25))
-                 for _ in array(Array_Filter):
+                 for _ in np_array(Array_Filter):
                      x = search(rf'^.*{_}.*', str(r.content))
                      if (x != None):
-                        for j in array(r.text.splitlines()):
+                        for j in np_array(r.text.splitlines()):
                             if (_ in j):
                                 Temp_Array = resplit("<dl>|<dt>|</dt>", j)
                                 if (len(Temp_Array) > 1):
-                                    for k in array(Temp_Array):
+                                    for k in np_array(Temp_Array):
                                         if (len(k) > 1):
                                             if (f'{i} - {k}' not in Array_Output): Array_Output.append(f'{i} - {k}')
 
