5553495ada4c1441540ae39b2c156f805867c9cc
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-11 23:48:05 +0200

    Update Threads_Scanning.py

--- Resources/Header_Files/Threads_Scanning.py
+++ Resources/Header_Files/Threads_Scanning.py
@@ -331,7 +331,7 @@ def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch
                                  Dict_Result['SSH'][url] = SSH_Vulns(url, t_seconds, Host_Name, Location)
                                  Dict_Temp['SSH'][url]   = Dict_Result['SSH'][url]
             except SSHException: Logs.Write_Log(url, Host_Name, join(Location, 'Logs'))
-            print (Dict_Temp['SSH'])
+            #print (Dict_Temp['SSH'])
 
             # Trace_End
             Logs.Trace_File(
