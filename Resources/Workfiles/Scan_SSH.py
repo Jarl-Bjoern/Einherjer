26d237c27633ae7d8e073500615637d3f971360d
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-11 23:18:58 +0200

    Update Scan_SSH.py

--- Resources/Workfiles/Scan_SSH.py
+++ Resources/Workfiles/Scan_SSH.py
@@ -84,10 +84,10 @@ def SSH_Vulns(url, Host_Name, Location, Dict_SSH_Version = {}, Dict_Temp = {}):
 
 
         if ('SSH-1' in str(Server_Banner)[::-(len(Server_Banner)-7)]):
-            Dict_SSH_Results['SSH_Banner']  = str(Server_Banner)
+            Dict_SSH_Results['SSH_Banner']  = str(Server_Banner)[:-2]
             Dict_SSH_Results['SSH_Version'] = '1'
         else:
-            Dict_SSH_Results['SSH_Banner']  = str(Server_Banner)
+            Dict_SSH_Results['SSH_Banner']  = str(Server_Banner)[:-2]
             Dict_SSH_Results['SSH_Version'] = '2'
     except TimeoutError:
         pass
