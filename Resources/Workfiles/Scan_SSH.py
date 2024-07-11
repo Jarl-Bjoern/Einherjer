010706374fb3c4879d78e6441a44686f0d1ccb6b
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-11 23:13:26 +0200

    Update Scan_SSH.py

--- Resources/Workfiles/Scan_SSH.py
+++ Resources/Workfiles/Scan_SSH.py
@@ -7,7 +7,7 @@ from ..Header_Files.Variables     import *
 from ..Standard_Operations.Logs   import Logs
 from ..Standard_Operations.Colors import Colors
 
-def SSH_Vulns(url, Host_Name, Location, Dict_SSH_Version = {}, Dict_SSH_Results = {'auth_methods': [], 'kex_algorithms': [], 'server_host_key_algorithms': [], 'encryption_algorithms': [], 'mac_algorithms': []}, Dict_Temp = {}):
+def SSH_Vulns(url, Host_Name, Location, Dict_SSH_Version = {}, Dict_Temp = {}):
     def Check_SSH_Values(List_With_Keys, Temp_Key = ""):
         Array_Temp = []
         for i in List_With_Keys:
@@ -17,7 +17,8 @@ def SSH_Vulns(url, Host_Name, Location, Dict_SSH_Version = {}, Dict_SSH_Results 
                 Array_Temp.append(Temp_Key)
         return Array_Temp
 
-    filter_values = r'x88|x01|x00|x15|xd5|\\|&|@openssh.com'
+    filter_values    = r'x88|x01|x00|x15|xd5|\\|&|@|none|x03|x0c|x14|x9f|x98|x7fl|xb4|xe6o|xa7|xee|x1b|xd8|x8c|x034|x92|~|x0f|x9d|xb8{|xf4|xe7|xcc|x89$Q|xef|xaa|xea|x93^|xe3|t:"F|/|openssh.com|xd1b+|xa0|xe8|xd9|x85|xba|xbf|lysator.liu.se'
+    Dict_SSH_Results = {'auth_methods': [], 'kex_algorithms': [], 'server_host_key_algorithms': [], 'encryption_algorithms': [], 'mac_algorithms': []} 
 
     # Split_Protocol
     if ('ssh://' in url):    URL = url.split('ssh://')[1]
@@ -43,12 +44,44 @@ def SSH_Vulns(url, Host_Name, Location, Dict_SSH_Version = {}, Dict_SSH_Results 
             except TypeError: Server_Banner = sock.recv(100)
 
             # Get_Ciphers
-            Dict_System = {}
+            Temp_Filter = []
             Ciphers = sock.recv(4096)
-            print (resplit(filter_values, str(Ciphers)))
             for _ in resplit(filter_values, str(Ciphers)):
-                if (_ != ''):
-                    print (_)
+                if (_ != '' and _[0] != 'x' and len(_) > 5):
+                    if (',' in _):
+                        Temp_Check = _.split(',')
+                        for Cipher_Check in Temp_Check:
+                            if (Cipher_Check != ''):
+                                cipher = Cipher_Check.lower()
+                                print (cipher)
+                                if ('ssh'   in cipher or
+                                    'ecdsa' in cipher or
+                                    'x509'  in cipher or
+                                    'pgp'   in cipher):
+                                          if (cipher not in Dict_SSH_Results['server_host_key_algorithms']):
+                                              Dict_SSH_Results['server_host_key_algorithms'].append(cipher)
+                                elif ('diffie-hellman'  in cipher or
+                                      'curve'           in cipher or
+                                      'sntrup'          in cipher or
+                                      'kex'             in cipher or
+                                      'ecdh'            in cipher):
+                                          if (cipher not in Dict_SSH_Results['kex_algorithms']):
+                                              Dict_SSH_Results['kex_algorithms'].append(cipher)
+                                elif ('hmac' in cipher or
+                                      'umac' in cipher):
+                                          if (cipher[0] == 'i'):
+                                                if (cipher not in Dict_SSH_Results['mac_algorithms']):
+                                                    Dict_SSH_Results['mac_algorithms'].append(cipher[1:])
+                                          else:
+                                                if (cipher not in Dict_SSH_Results['mac_algorithms']):
+                                                    Dict_SSH_Results['mac_algorithms'].append(cipher)
+                                elif ('aes'     in cipher or
+                                      'arcfour' in cipher or
+                                      'cbc'     in cipher or
+                                      'chacha'  in cipher):
+                                          if (cipher not in Dict_SSH_Results['encryption_algorithms']):
+                                              Dict_SSH_Results['encryption_algorithms'].append(cipher)
+
 
         if ('SSH-1' in str(Server_Banner)[::-(len(Server_Banner)-7)]):
             print (Server_Banner)
@@ -63,19 +96,11 @@ def SSH_Vulns(url, Host_Name, Location, Dict_SSH_Version = {}, Dict_SSH_Results 
     async def check_auth(Target):
         return await get_server_auth_methods(Target)
 
-    # Experimental
-    #opts        = Transport(Target, int(Port)).get_security_options()
-    #Dict_System['kex_algorithms']             = Check_SSH_Values(opts.kex)
-    #Dict_System['server_host_key_algorithms'] = Check_SSH_Values(opts.key_types)
-    #Dict_System['encryption_algorithms']      = Check_SSH_Values(opts.ciphers)
-    #Dict_System['mac_algorithms']             = Check_SSH_Values(opts.digests)
-    #print(opts.compression)
-
     # Start_Scans
     try:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        Dict_System['auth_methods'] = loop.run_until_complete(check_auth(Target))
+        Dict_SSH_Results['auth_methods'] = loop.run_until_complete(check_auth(Target))
     except (AsyncSSHError, OSError) as e:
         exit(f'SSH connection failed: {str(e)}')
 
