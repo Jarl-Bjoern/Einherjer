8c5be1ff6c8716cbe1bc58ed181d558e721f69c8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-12 10:52:11 +0200

    Create Scan_SMB.py

---
+++ Resources/Workfiles/Scan_SMB.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
+# Libraries
+from ..Header_Files.Variables     import *
+from ..Standard_Operations.Logs   import Logs
+from ..Standard_Operations.Colors import Colors
