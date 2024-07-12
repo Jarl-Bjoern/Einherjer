cddb5c3f637ea80f3324cfee57ff80dc4b35fa64
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-12 17:09:24 +0200

    Update Threads_Scanning.py

--- Resources/Header_Files/Threads_Scanning.py
+++ Resources/Header_Files/Threads_Scanning.py
@@ -329,7 +329,6 @@ def Thread_Scanning_Start(url, t_seconds, queue, dict_switch, screen_dir, switch
             # Scanning_Process
             Dict_Result['SSH'][url] = SSH_Vulns(url, t_seconds, Host_Name, Location)
             Dict_Temp['SSH'][url]   = Dict_Result['SSH'][url]
-            #print (Dict_Temp['SSH'])
 
             # Trace_End
             Logs.Trace_File(
