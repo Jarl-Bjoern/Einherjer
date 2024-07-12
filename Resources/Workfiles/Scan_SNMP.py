5af73e3e65608668f5d20a19b91705937125c46e
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-12 18:50:25 +0200

    Update Scan_SNMP.py

--- Resources/Workfiles/Scan_SNMP.py
+++ Resources/Workfiles/Scan_SNMP.py
@@ -11,25 +11,46 @@ from ..Standard_Operations.Standard import Standard
 class Check_SNMP:
     def Basic_Check(url, Dict_Temp = {}):
         for oid in Array_OIDs:
+            # SNMPv1
             if ('iso' in oid):
-                g = getCmd(
-                        SnmpEngine(),
-                        CommunityData('Public', mpModel=0),
-                        UdpTransportTarget((f'{url}', 161)),
-                        ContextData(),
-                        ObjectType(ObjectIdentity(oid)),
-                        lexicographicMode=False
+                Temp_Output = getCmd(
+                                SnmpEngine(),
+                                CommunityData('Public', mpModel=0),
+                                UdpTransportTarget((f'{url}', 161)),
+                                ContextData(),
+                                ObjectType(ObjectIdentity(oid)),
+                                lexicographicMode=False
                 )
             else:
-                g = nextCmd(
-                        SnmpEngine(),
-                        CommunityData('Public', mpModel=0),
-                        UdpTransportTarget(('{url}', 161)),
-                        ContextData(),
-                        ObjectType(ObjectIdentity(oid)),
-                        lexicographicMode=False
+                Temp_Output = nextCmd(
+                                SnmpEngine(),
+                                CommunityData('Public', mpModel=0),
+                                UdpTransportTarget(('{url}', 161)),
+                                ContextData(),
+                                ObjectType(ObjectIdentity(oid)),
+                                lexicographicMode=False
                 )
-    
+
+            # SNMPv2
+            if (Temp_Output == None):
+                if ('iso' in oid):
+                    Temp_Output = getCmd(
+                                    SnmpEngine(),
+                                    CommunityData('Public', mpModel=1),
+                                    UdpTransportTarget((f'{url}', 161)),
+                                    ContextData(),
+                                    ObjectType(ObjectIdentity(oid)),
+                                    lexicographicMode=False
+                    )
+                else:
+                    Temp_Output = nextCmd(
+                                    SnmpEngine(),
+                                    CommunityData('Public', mpModel=1),
+                                    UdpTransportTarget(('{url}', 161)),
+                                    ContextData(),
+                                    ObjectType(ObjectIdentity(oid)),
+                                    lexicographicMode=False
+                    )
         
             for Indication_Error, Status_Error, Index_Error, Bind_Variables in Temp_Output:
                 for _ in Bind_Variables:
