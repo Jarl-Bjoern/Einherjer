0a335ae077172020250e47acb1aab31b1bc4bbb2
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-12 19:37:32 +0200

    Update Scan_SNMP.py

--- Resources/Workfiles/Scan_SNMP.py
+++ Resources/Workfiles/Scan_SNMP.py
@@ -50,11 +50,11 @@ class Check_SNMP:
         Temp_Output = None
         for oid in Array_OIDs:
             # SNMPv1
-            Temp_Output = Walk_SNMP(oid, url, 0, 'Public')
+            Temp_Output = Walk_SNMP(oid, Target, 0, 'Public')
 
             # SNMPv2
             if (Temp_Output == None):
-                Temp_Output = Walk_SNMP(oid, url, 1, 'Public')
+                Temp_Output = Walk_SNMP(oid, Target, 1, 'Public')
 
             for Indication_Error, Status_Error, Index_Error, Bind_Variables in Temp_Output:
                 for _ in Bind_Variables:
@@ -64,18 +64,54 @@ class Check_SNMP:
 
 
     def String_Enumeration(url, t_seconds, Host_Name, Location, Dict_System = {}):
-        for _ in Array_Community_Strings:
-            Temp_Output = getCmd(
+        def Walk_SNMP(oid, url, snmp_version, community_string, t_seconds):
+            Temp_Output = ""
+            if ('iso' in oid):
+                Temp_Output = getCmd(
                                 SnmpEngine(),
-                                CommunityData(i, mpModel=0),
+                                CommunityData(community_string, mpModel=snmp_version),
                                 UdpTransportTarget((url, 161), timeout=2.0, retries=1),
                                 ContextData(),
-                                ObjectType(ObjectIdentity('iso.3.6.1.2.1.1.5.0')),
+                                ObjectType(ObjectIdentity(oid)),
                                 lexicographicMode=False
-            )
-        
+                )
+            else:
+                Temp_Output = nextCmd(
+                                SnmpEngine(),
+                                CommunityData(community_string, mpModel=snmp_version),
+                                UdpTransportTarget((url, 161), timeout=2.0, retries=1),
+                                ContextData(),
+                                ObjectType(ObjectIdentity(oid)),
+                                lexicographicMode=False
+                )
+            return Temp_Output
+
+        # Split_Protocol
+        if ('snmp://' in url):    URL = url.split('snmp://')[1]
+    
+        # Port_Filter
+        if (url.count(':') > 1): Port = URL.split(':')[1]
+        else:                    Port = 22
+    
+        # Get_Only_Target
+        if (':' in URL):         Target = URL.split(':')[0]
+        else:                    Target = URL
+    
+        # Get_Host_Name
+        if (Host_Name != ""):    Dict_System['DNS'] = Host_Name
+        else:                    Dict_System['DNS'] = ""
+
+        for _ in Array_Community_Strings:        
+            # SNMPv1
+            Temp_Output = Walk_SNMP('iso.3.6.1.2.1.1.5.0', Target, 0, _)
+
+            # SNMPv2
+            if (Temp_Output == None):
+                Temp_Output = Walk_SNMP('iso.3.6.1.2.1.1.5.0', Target, 1, _)
+
             for errorIndication, errorStatus, errorIndex, varBinds in Temp_Output:
                        if (errorIndication == None):
-                           print (i)
+                           print (_)
             break
 
+        return Dict_System
