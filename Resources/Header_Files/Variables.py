fdced7546dc19f7e4541a43d3e69d0901e3417a9
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-12 10:01:57 +0200

    Update Variables.py

--- Resources/Header_Files/Variables.py
+++ Resources/Header_Files/Variables.py
@@ -22,16 +22,16 @@ if (Program_Mode == "Scanning_Mode"):
             exit(Colors.RED+"It's not possible to use both http_header templates at the same time."+Colors.RESET)
     else:   Dict_Header = {}
 
-    Array_CORS_Header                   = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_cors_header', 'http')
-    Array_Deprecated_Header             = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_deprecated_header', 'http')
-    Array_HTTP_Methods                  = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_methods', 'http')
-    Array_Information_Disclosure_Header = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_information_disclosure_header', 'http')
-    Array_Security_Flags                = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'cookie_flags', 'http')
-    Array_TLS_Algorithms                = Standard.Read_Template(dirname(realpath(__file__)).replace('Resources/Header_Files', "Config/ssl_ciphers.txt"))
-    Dict_Custom_Header                  = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', "Config/http_config.yaml"), 'http_custom_header', 'json')
+    Array_CORS_Header                      = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_cors_header', 'http')
+    Array_Deprecated_Header                = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_deprecated_header', 'http')
+    Array_HTTP_Methods                     = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_methods', 'http')
+    Array_Information_Disclosure_Header    = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'http_information_disclosure_header', 'http')
+    Array_Security_Flags                   = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', 'Config/http_config.yaml'), 'cookie_flags', 'http')
+    Array_TLS_Algorithms                   = Standard.Read_Template(dirname(realpath(__file__)).replace('Resources/Header_Files', "Config/ssl_ciphers.txt"))
+    Dict_Custom_Header                     = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', "Config/http_config.yaml"), 'http_custom_header', 'json')
 
     # Variables
-    COLOR_Headline       = "black"
+    COLOR_Headline                         = "black"
 
 if (Program_Mode == "Scanning_Mode" or Program_Mode == "Filter_Mode"):
     Array_SSH_Algorithms                   = Standard.Read_YAML_Config_File(dirname(realpath(__file__)).replace('Resources/Header_Files', "Config/ssh_ciphers.yaml"), "empty", 'ssh')
