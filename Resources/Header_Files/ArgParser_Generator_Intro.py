a48c5ed51fdfde4a611c37f4f9ab8b5e16b578de
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-13 11:34:08 +0200

    Update ArgParser_Generator_Intro.py

--- Resources/Header_Files/ArgParser_Generator_Intro.py
+++ Resources/Header_Files/ArgParser_Generator_Intro.py
@@ -34,9 +34,7 @@ def Argument_Parser(Copyright_Year, Template_Location = dirname(realpath(__file_
 
     generator_arguments.add_argument('-gMf', '--generate-malicious-files', type=bool, nargs='?', const=True, help=Colors.GREEN+'This Parameter creates malicious files to farm hashes.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
-    malicious_arguments.add_argument('-mFt', '--malicious-file-types', choices=['all','ini','lnk','scf','url'], type=str, default="all", help=Colors.GREEN+'This Parameter creates malicious files to farm hashes.\n\nDefault: all'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
     malicious_arguments.add_argument('-mT', '--malicious-target', type=str, nargs='*', help=Colors.GREEN+'This Parameter sets the attack host for the malicious files.\n'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
-    malicious_arguments.add_argument('-mFn', '--malicious-file-name', type=str, help=Colors.GREEN+'This Parameter sets the name for the malicious file.\n'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
     optional.add_argument('-h','--help', action='help', default=SUPPRESS, help=Colors.GREEN+'Show this help message and exit.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
