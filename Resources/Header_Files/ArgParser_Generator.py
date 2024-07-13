c3ea17b1e0689b6fa2fe37539ab9ab145e813e6a
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-13 10:47:20 +0200

    Update ArgParser_Generator.py

--- Resources/Header_Files/ArgParser_Generator.py
+++ Resources/Header_Files/ArgParser_Generator.py
@@ -24,13 +24,16 @@ def Argument_Parser(Copyright_Year, Template_Location = dirname(realpath(__file_
     config_arguments    = parser.add_argument_group(Colors.ORANGE+'config arguments'+Colors.RESET)
     debug_arguments     = parser.add_argument_group(Colors.ORANGE+'debug arguments'+Colors.RESET)
     generator_arguments = parser.add_argument_group(Colors.ORANGE+'format arguments'+Colors.RESET)
+    target_arguments    = parser.add_argument_group(Colors.ORANGE+'format arguments'+Colors.RESET)
     optional            = parser.add_argument_group(Colors.ORANGE+'optional arguments'+Colors.RESET)
 
     config_arguments.add_argument('-o', '--output-location', type=str, help=Colors.GREEN+'Specify the location where the result should be saved.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
     debug_arguments.add_argument('-d', '--debug', type=bool, nargs='?', default=False, help=Colors.GREEN+'This Parameter deactivates the terminal clearing after starting the tool.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
-    debug_arguments.add_argument('-gMf', '--generate-malicious-files', choices=['all','ini','inf','lnk','scf','url'], type=str, default="all", help=Colors.GREEN+'This Parameter creates malicious files to farm hashes.\n\nDefault: all'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+    generator_arguments.add_argument('-gMf', '--generate-malicious-files', choices=['all','ini','inf','lnk','scf','url'], type=str, default="all", help=Colors.GREEN+'This Parameter creates malicious files to farm hashes.\n\nDefault: all'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
+
+    target_arguments.add_argument('-mT', '--malicious-target', type=str, nargs='*', help=Colors.GREEN+'This Parameter sets the attack host for the malicious files.\n'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
     optional.add_argument('-h','--help', action='help', default=SUPPRESS, help=Colors.GREEN+'Show this help message and exit.'+Colors.BLUE+'\n\n-------------------------------------------------------------------------------------'+Colors.RESET)
 
