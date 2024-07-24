ce6b9e3426883e16efb7c2b6fa7440d3ce261679
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-24 18:03:31 +0200

    Update Scan_SSL.py

--- Resources/Workfiles/Scan_SSL.py
+++ Resources/Workfiles/Scan_SSL.py
@@ -176,15 +176,9 @@ def SSL_Vulns(array_ssl_targets, ssl_timeout, Location, Array_Result_Filter = ['
                 temp_json_output = json_output.json(sort_keys=True, indent=4, ensure_ascii=True)
 
                 # Output_Backup
-                Backup_Out, Output_File_Name = join(Location, 'SSL_Backup'), "SSL_Scan_Out_0.json"
+                Backup_Out, Output_File_Name = join(Location, 'SSL_Backup'), f"SSL_Scan_Out_{URL}_{Port}.json"
                 if (not exists(Backup_Out)):
                     makedirs(Backup_Out)
-                    Backup_Val = Output_File_Name[:-5].split('_')[-1:][0]
-
-                if (len(listdir(Backup_Out)) > 0):
-                    Counter_Name     = int(Backup_Val)+1
-                    Output_File_Name = Output_File_Name.replace(str(Backup_Val), str(Counter_Name))
-                    Backup_Val       = Counter_Name
 
                 with open(join(Backup_Out, Output_File_Name), 'w', encoding='UTF-8') as f:
                     f.write(temp_json_output)
