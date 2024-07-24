ec2c49ac77792a2d622a0f92d6fa163a6ffeb256
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-07-24 17:59:27 +0200

    Update Scan_SSL.py

--- Resources/Workfiles/Scan_SSL.py
+++ Resources/Workfiles/Scan_SSL.py
@@ -183,7 +183,7 @@ def SSL_Vulns(array_ssl_targets, ssl_timeout, Location, Array_Result_Filter = ['
 
                 if (len(listdir(Backup_Out)) > 0):
                     Counter_Name     = int(Backup_Val)+1
-                    Output_File_Name = Output_File_Name.replace(Backup_Val, str(Counter_Name))
+                    Output_File_Name = Output_File_Name.replace(str(Backup_Val), str(Counter_Name))
                     Backup_Val       = Counter_Name
 
                 with open(join(Backup_Out, Output_File_Name), 'w', encoding='UTF-8') as f:
