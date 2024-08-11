2485b01ad3947ff0a09425eb625b0288afe4b910
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2024-08-11 18:04:24 +0200

    Update HTML.py

--- Resources/Format/HTML.py
+++ Resources/Format/HTML.py
@@ -31,7 +31,7 @@ table, th, td {
     Head_HTML += "<th><font color='Orange'>Host</font></th>"
     Head_HTML += "<th><font color='Orange'>Screenshot</font></th>\n</tr>"
     for Body in listdir(location):
-        Body_HTML = "<tr>"
+        Body_HTML += "<tr>"
         Body_HTML += f"<td><center><font color='Orange'>{Body.split('(')[1].split(')')[0].replace('_', ':')}</font></center></td>\n"
         Body_HTML += f"<td><img class='center' src='{join(location, Body)}' width=800/></td>\n"
         Body_HTML += "</tr>\n"
