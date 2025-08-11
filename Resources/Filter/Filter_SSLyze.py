7cb8abee6f56033cca24e851dc57e81697baf6e7
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2025-08-11 17:46:06 +0200

    Update Filter_SSLyze.py

--- Resources/Filter/Filter_SSLyze.py
+++ Resources/Filter/Filter_SSLyze.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Rainer Christian Bjoern Herold
+
 # Libraries
 from ..Header_Files.Variables     import *
 from ..Standard_Operations.Logs   import Logs
